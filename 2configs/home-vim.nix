3a9a275c4d7eba1b8064aefa447820f7dfc0e88a
Enno Lohmeier <enno@nerdworks.de>
2019-11-06 10:08:42 +0100

    add vim

---
+++ 2configs/home-vim.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+with import <ptsd/lib>;
+
+let
+  vim = pkgs.callPackage ../5pkgs/vim-customized {};
+in
+{
+  home.sessionVariables = {
+    EDITOR = "${vim}/bin/vim";
+  };
+
+  home.packages = [ vim ];
+}
