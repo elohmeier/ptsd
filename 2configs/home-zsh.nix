bee5f892f57afbb5a472d9760cc3739b53938e4a
Enno Lohmeier <enno@nerdworks.de>
2019-11-06 09:59:23 +0100

    add fzf & z-lua

--- 2configs/home-zsh.nix
+++ 2configs/home-zsh.nix
@@ -5,15 +5,40 @@ let
   shellAliases = import ./aliases.nix;
 in
 {
-  home.packages = [
-    pkgs.fzf
-  ];
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.z-lua = {
+    enable = true;
+    enableZshIntegration = true;
+  };
 
   programs.zsh = {
     enable = true;
 
     initExtra = ''
-      source "$(${pkgs.fzf}/bin/fzf-share)/key-bindings.zsh"
+          if [ "$TERM" != dumb ]; then
+      autoload -U colors && colors
+      PS1="%B%(?..[%?] )%b%(!.%{$fg_bold[red]%}%m.%{$fg_bold[green]%}%n@%m) %{$fg_bold[blue]%}%~%{$reset_color%} "
+
+      function preexec() {
+        timer=''${timer:-$SECONDS}
+      }
+
+      function precmd() {
+        if [ $timer ]; then
+          timer_show=$(($SECONDS - $timer))
+          if [ $timer_show -lt 2 ]; then
+            export RPROMPT=""
+          else
+            export RPROMPT="%F{cyan}''${timer_show}s %{$reset_color%}"
+          fi
+          unset timer
+        fi
+      }
+          fi
     '';
 
     shellAliases = shellAliases;
