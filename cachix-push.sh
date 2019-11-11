ba63c11c69ac1eb962f4f602be6229e22c622fa0
Enno Lohmeier <enno@nerdworks.de>
2019-11-11 10:53:09 +0100

    improve cachix-push script

--- cachix-push.sh
+++ cachix-push.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-CACHIX_SIGNING_KEY=`pass cachix/ws1`
+set -e
+
+export CACHIX_SIGNING_KEY=`pass cachix/ws1`
 
 echo "Building and pushing 5pkgs/vim-customized..."
 nix-build -E 'with import <nixpkgs> {}; callPackage ./5pkgs/vim-customized {}' | cachix push nerdworks
