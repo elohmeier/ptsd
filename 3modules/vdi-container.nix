9f50ca102d514fb19b449578e48c1f933f86c0ba
Enno Lohmeier <enno@nerdworks.de>
2019-11-11 20:47:06 +0100

    vdi-container additions WIP

--- 3modules/vdi-container.nix
+++ 3modules/vdi-container.nix
@@ -8,6 +8,18 @@ with lib;
 
 let
   cfg = config.ptsd.vdi-container;
+  xephyrWrapper = pkgs.writeShellScriptBin "${cfg.containerName}-xephyr" ''
+    ${pkgs.xorg.xorgserver}/bin/Xephyr \
+      -resizeable \
+      -br \
+      -ac \
+      -noreset \
+      -screen 1920x1080 \
+      -extension MIT-SHM -extension XTEST \
+      -nolisten tcp \
+      -keybd ephyr,xkbmodel=evdev,xkblayout='de(nodeadkeys)' \
+      ${cfg.xephyrDisplayId}
+  '';
 in
 {
   options.ptsd.vdi-container = {
@@ -19,6 +31,7 @@ in
     };
     containerName = mkOption {
       type = types.str;
+      default = "my-vdi";
     };
     containerAddress = mkOption {
       type = types.str;
@@ -30,6 +43,15 @@ in
       default = "192.168.100.10";
       description = "IP address of the host in the private host/container-network";
     };
+    xephyrDisplayId = mkOption {
+      type = types.str;
+      default = ":1";
+    };
+    startupUrls = mkOption {
+      type = types.listOf (types.str);
+      description = "URLs Firefox should open automatically after startup, e.g. Login web page for Firewalls etc.";
+      default = [];
+    };
   };
 
   config = mkIf cfg.enable {
@@ -38,13 +60,15 @@ in
       nat = {
         enable = true;
         internalInterfaces = [ "ve-+" ];
-        externalInterfaces = cfg.extIf;
+        externalInterface = cfg.extIf;
       };
 
       extraHosts = ''
         ${cfg.containerAddress} ${cfg.containerName}
       '';
     };
+
+    environment.systemPackages = [ xephyrWrapper ];
 
     containers."${cfg.containerName}" = {
       autoStart = true;
@@ -63,6 +87,25 @@ in
         { config, pkgs, ... }:
           {
             boot.isContainer = true;
+
+            environment.variables = {
+              DISPLAY = cfg.xephyrDisplayId;
+              ICEWM_PRIVCFG = "/etc/X11/icewm";
+            };
+
+            environment.etc."X11/icewm/startup" = {
+              text = ''
+                ${pkgs.firefox}/bin/firefox \
+                  --no-remote \
+                  ${lib.concatMapStrings (x: " \"${x}\"") cfg.startupUrls}
+              '';
+              mode = "0755";
+            };
+
+            networking = {
+              useHostResolvConf = false;
+              nameservers = [ "8.8.8.8" "8.8.4.4" ]; # will be used for VPN DNS lookup
+            };
           };
     };
 
