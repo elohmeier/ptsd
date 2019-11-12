01fd0d40c3394dddd0a2462a5dc2115af7d8be10
Enno Lohmeier <enno@nerdworks.de>
2019-11-12 23:22:22 +0100

    apu2: add physical.nix

---
+++ 1systems/apu2/physical.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    <ptsd/2configs/hw/apu2.nix>
+  ];
+
+  fileSystems."/" =
+    {
+      device = "/dev/disk/by-uuid/297096e3-cf98-4401-87cf-2831f6995139";
+      fsType = "ext4";
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/a5a9cdaf-107d-4ee2-b7eb-6bc65edd7a89"; } ];
+}
