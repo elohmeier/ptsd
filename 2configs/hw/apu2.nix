8c0e11690e31ab35bd9f207a5880ae7731184399
Enno Lohmeier <enno@nerdworks.de>
2019-11-12 23:04:48 +0100

    add apu2/rpi3b+ hw

---
+++ 2configs/hw/apu2.nix
@@ -0,0 +1,17 @@
+# PC-Engines APU2c4
+
+{ config, lib, pkgs, ... }:
+
+{
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "ehci_pci" "usb_storage" "sd_mod" "sdhci_pci" ];
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sda";
+
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelParams = [ "console=tty0" "console=ttyS0,115200n8" ];
+  boot.extraModulePackages = [];
+
+  nix.maxJobs = lib.mkDefault 4;
+}
