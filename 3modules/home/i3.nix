ca28e915cb9f8ee160e269fdac49e1053c64dd26
Enno Lohmeier <enno@nerdworks.de>
2019-11-12 22:54:54 +0100

    i3: configurable alacritty color theme

--- 3modules/home/i3.nix
+++ 3modules/home/i3.nix
@@ -60,80 +60,81 @@ let
     }
   ''}
   '';
-  # alacritty themes
-  solarized_light = {
+  alacritty_themes = {
+    solarized_light = {
 
-    # Default colors
-    primary = {
-      background = "0xfdf6e3"; # base3
-      foreground = "0x657b83"; # base00
-    };
+      # Default colors
+      primary = {
+        background = "0xfdf6e3"; # base3
+        foreground = "0x657b83"; # base00
+      };
 
-    # Cursor colors
-    cursor = {
-      text = "0xfdf6e3"; # base3
-      cursor = "0x657b83"; # base00
-    };
+      # Cursor colors
+      cursor = {
+        text = "0xfdf6e3"; # base3
+        cursor = "0x657b83"; # base00
+      };
 
-    # Normal colors
-    normal = {
-      black = "0x073642"; # base02
-      red = "0xdc322f"; # red
-      green = "0x859900"; # green
-      yellow = "0xb58900"; # yellow
-      blue = "0x268bd2"; # blue
-      magenta = "0xd33682"; # magenta
-      cyan = "0x2aa198"; # cyan
-      white = "0xeee8d5"; # base2
-    };
+      # Normal colors
+      normal = {
+        black = "0x073642"; # base02
+        red = "0xdc322f"; # red
+        green = "0x859900"; # green
+        yellow = "0xb58900"; # yellow
+        blue = "0x268bd2"; # blue
+        magenta = "0xd33682"; # magenta
+        cyan = "0x2aa198"; # cyan
+        white = "0xeee8d5"; # base2
+      };
 
-    # Bright colors
-    bright = {
-      black = "0x002b36"; # base03
-      red = "0xcb4b16"; # orange
-      green = "0x586e75"; # base01
-      yellow = "0x657b83"; # base00
-      blue = "0x839496"; # base0
-      magenta = "0x6c71c4"; # violet
-      cyan = "0x93a1a1"; # base1
-      white = "0xfdf6e3"; # base3
+      # Bright colors
+      bright = {
+        black = "0x002b36"; # base03
+        red = "0xcb4b16"; # orange
+        green = "0x586e75"; # base01
+        yellow = "0x657b83"; # base00
+        blue = "0x839496"; # base0
+        magenta = "0x6c71c4"; # violet
+        cyan = "0x93a1a1"; # base1
+        white = "0xfdf6e3"; # base3
+      };
     };
-  };
-  solarized_dark = {
-    # Default colors
-    primary = {
-      background = "0x002b36"; # base03
-      foreground = "0x839496"; # base0
-    };
+    solarized_dark = {
+      # Default colors
+      primary = {
+        background = "0x002b36"; # base03
+        foreground = "0x839496"; # base0
+      };
 
-    # Cursor colors
-    cursor = {
-      text = "0x002b36"; # base03
-      cursor = "0x839496"; # base0
-    };
+      # Cursor colors
+      cursor = {
+        text = "0x002b36"; # base03
+        cursor = "0x839496"; # base0
+      };
 
-    # Normal colors
-    normal = {
-      black = "0x073642"; # base02
-      red = "0xdc322f"; # red
-      green = "0x859900"; # green
-      yellow = "0xb58900"; # yellow
-      blue = "0x268bd2"; # blue
-      magenta = "0xd33682"; # magenta
-      cyan = "0x2aa198"; # cyan
-      white = "0xeee8d5"; # base2
-    };
+      # Normal colors
+      normal = {
+        black = "0x073642"; # base02
+        red = "0xdc322f"; # red
+        green = "0x859900"; # green
+        yellow = "0xb58900"; # yellow
+        blue = "0x268bd2"; # blue
+        magenta = "0xd33682"; # magenta
+        cyan = "0x2aa198"; # cyan
+        white = "0xeee8d5"; # base2
+      };
 
-    # Bright colors
-    bright = {
-      black = "0x002b36"; # base03
-      red = "0xcb4b16"; # orange
-      green = "0x586e75"; # base01
-      yellow = "0x657b83"; # base00
-      blue = "0x839496"; # base0
-      magenta = "0x6c71c4"; # violet
-      cyan = "0x93a1a1"; # base1
-      white = "0xfdf6e3"; # base3
+      # Bright colors
+      bright = {
+        black = "0x002b36"; # base03
+        red = "0xcb4b16"; # orange
+        green = "0x586e75"; # base01
+        yellow = "0x657b83"; # base00
+        blue = "0x839496"; # base0
+        magenta = "0x6c71c4"; # violet
+        cyan = "0x93a1a1"; # base1
+        white = "0xfdf6e3"; # base3
+      };
     };
   };
 in
@@ -167,6 +168,10 @@ in
       type = types.bool;
       default = true;
       description = "Configure alacritty and setup i3 integration";
+    };
+    alacrittyTheme = mkOption {
+      type = types.str;
+      default = "solarized_dark";
     };
     monospaceFont = mkOption {
       type = types.str;
@@ -331,8 +336,7 @@ in
       enable = true;
       settings = {
         font.size = 11.0;
-        #colors = solarized_dark;
-        colors = solarized_light;
+        colors = alacritty_themes."${cfg.alacrittyTheme}";
       };
     };
   };
