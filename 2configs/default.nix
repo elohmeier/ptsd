14a44a82a4604e01b7c3ac7074ed8b0e340d00d2
Enno Lohmeier <enno@nerdworks.de>
2019-11-23 18:56:08 +0100

    update default cfg

--- 2configs/default.nix
+++ 2configs/default.nix
@@ -1,4 +1,31 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
+
+let
+  sshPubKeys = import ./ssh-pubkeys.nix;
+in
 {
-  imports = [];
+
+  environment.shellAliases = import ./aliases.nix;
+
+  users = {
+    mutableUsers = false;
+
+    users.root = {
+      shell = pkgs.bash;
+      openssh.authorizedKeys.keys = [
+        sshPubKeys.sshPub.nw1
+        sshPubKeys.sshPub.nw15_terminus
+        sshPubKeys.sshPub.nw30
+        sshPubKeys.sshPub.enno_yubi41
+        sshPubKeys.sshPub.enno_yubi49
+      ];
+    };
+  };
+
+  # pubkeys
+  # hostkeys
+  # timezone
+  # ssh
+  # ...
+
 }
