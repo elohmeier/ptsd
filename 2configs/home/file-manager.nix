7eb4990d858c330aab4842359752f7a796f8ee94
Enno Lohmeier <enno@nerdworks.de>
2020-01-08 12:37:40 +0100

    file-manager: add vim

--- 2configs/home/file-manager.nix
+++ 2configs/home/file-manager.nix
@@ -1,7 +1,6 @@
 { config, lib, pkgs, ... }:
 
 {
-
   xdg.dataFile = {
 
     "file-manager/actions/nobbofin_assign_fzf.desktop".text = lib.generators.toINI {} {
@@ -32,6 +31,23 @@
       };
     };
 
-  };
+    "applications/vim.desktop" = {
+      text = lib.generators.toINI {} {
+        "Desktop Entry" = {
+          Name = "Vim";
+          Comment = "Edit text files in a console using Vim";
+          TryExec = "vim";
+          Exec = "vim %F";
+          Terminal = true;
+          Type = "Application";
+          Icon = "${pkgs.tango-icon-theme}/share/icons/Tango/scalable/apps/text-editor.svg";
+          Categories = "Application;Utility;TextEditor;";
+          StartupNotify = false;
+          MimeType = "text/english;text/plain;text/x-makefile;text/x-c++hdr;text/x-c++src;text/x-chdr;text/x-csrc;text/x-java;text/x-moc;text/x-pascal;text/x-tcl;text/x-tex;application/x-shellscript;text/x-c;text/x-c++;";
+        };
+      };
+      onChange = "${pkgs.desktop-file-utils}/bin/update-desktop-database ${config.xdg.dataHome}/applications/";
+    };
 
+  };
 }
