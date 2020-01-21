f51bd33105f1b4815118f5d18ceefd15c7f189e2
Enno Lohmeier <enno@nerdworks.de>
2020-01-21 11:06:38 +0100

    tp1: use systemd-resolved

--- 1systems/tp1/config.nix
+++ 1systems/tp1/config.nix
@@ -94,8 +94,11 @@
     useDHCP = false;
   };
 
+  services.resolved.enable = true;
+
   networking.networkmanager = {
     enable = true;
+    dns = "systemd-resolved";
     wifi = {
       macAddress = "random";
       powersave = true;
