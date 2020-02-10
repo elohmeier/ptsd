86d6346565c5dacdf5602d3d233584089ace6e83
Enno Lohmeier <enno@nerdworks.de>
2020-02-10 19:51:52 +0100

    nwlock: fix dependency

--- 5pkgs/nwlock/default.nix
+++ 5pkgs/nwlock/default.nix
@@ -1,6 +1,6 @@
 { pkgs }:
 pkgs.writeDashBin "nwlock" ''
-  XY=$(${pkgs.xorg.xrandr}/bin/xrandr --current | grep '*' | uniq | head -n 1 | awk '{print $1}')
+  XY=$(${pkgs.xorg.xrandr}/bin/xrandr --current | grep '*' | uniq | head -n 1 | ${pkgs.gawk}/bin/awk '{print $1}')
 
   ${pkgs.imagemagick}/bin/convert ${<ci/os/Nerdworks_Hamburg_Logo_Web_Negativ_Weiss.png>} -background black -gravity center -extent $XY RGB:- | ${pkgs.i3lock}/bin/i3lock --color=000000 --image /dev/stdin --raw "$XY:rgb"
 
