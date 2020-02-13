00398193ec5c74bef4e83de6566ec6593d6e97ed
Enno Lohmeier <enno@nerdworks.de>
2020-02-13 10:00:06 +0100

    git-alarm: remove post-update hook

--- 5pkgs/git-alarm/default.nix
+++ 5pkgs/git-alarm/default.nix
@@ -8,10 +8,11 @@ in
 python3Packages.buildPythonApplication {
   name = "git-alarm";
   src = ./.;
+
+  # unfortunately there is no post-push hook, so we can't react to that easily
   postInstall = ''
     mkdir -p $out/share/hooks
     ln -s ${runGitAlarm} $out/share/hooks/post-commit
     ln -s ${runGitAlarm} $out/share/hooks/post-merge
-    ln -s ${runGitAlarm} $out/share/hooks/post-update
   '';
 }
