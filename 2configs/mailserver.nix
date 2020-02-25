00ab370908a56e7e1fdd48a124f976fdb88d97a2
Enno Lohmeier <enno@nerdworks.de>
2020-02-25 13:29:23 +0100

    mailserver: add monit config

--- 2configs/mailserver.nix
+++ 2configs/mailserver.nix
@@ -1,5 +1,8 @@
 { config, lib, pkgs, ... }:
 
+let
+  fqdn = "htz2.host.nerdworks.de"; # has reverse DNS
+in
 {
   imports = [
     (
@@ -12,7 +15,7 @@
 
   mailserver = {
     enable = true;
-    fqdn = "htz2.host.nerdworks.de"; # has reverse DNS
+    fqdn = fqdn;
     domains = [ "nerdworks.de" "nerd-works.de" ];
 
     loginAccounts = {};
@@ -25,5 +28,23 @@
 
     virusScanning = false;
   };
+
+  ptsd.nwmonit.extraConfig = [
+    ''
+      check process postfix matching "${pkgs.postfix}/libexec/postfix/master"
+        start program = "${pkgs.systemd}/bin/systemctl start postfix"
+        stop program = "${pkgs.systemd}/bin/systemctl stop postfix"
+        if failed host ${fqdn} port 25 protocol smtp for 5 cycles then restart
+
+      check process dovecot with pidfile /var/run/dovecot2/master.pid
+        start program = "${pkgs.systemd}/bin/systemctl start dovecot2"
+        stop program = "${pkgs.systemd}/bin/systemctl stop dovecot2"
+        if failed host ${fqdn} port 143 protocol imap for 5 cycles then restart
+
+      check process rspamd matching "rspamd: main process"
+        start program = "${pkgs.systemd}/bin/systemctl start rspamd"
+        stop program = "${pkgs.systemd}/bin/systemctl stop rspamd"
+    ''
+  ];
 
 }
