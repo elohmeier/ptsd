e1f65474f24b60d096e7b6d14a7bfc477ebea793
Enno Lohmeier <enno@nerdworks.de>
2020-04-11 19:48:08 +0200

    nwbackup-osx: add scripts

--- 5pkgs/nwbackup-osx/default.nix
+++ 5pkgs/nwbackup-osx/default.nix
@@ -1,30 +1,45 @@
-{ writers, borgbackup, targetHost, targetDomain ? "host.nerdworks.de" }:
+{ writers, symlinkJoin, borgbackup, targetHost, targetDomain ? "host.nerdworks.de" }:
 let
   universe = import <ptsd/2configs/universe.nix>;
+
+  backup-script = writers.writeDashBin "nwbackup-osx-${target}" ''
+    set -e
+
+    export BORG_REPO="borg-$(hostname)@${target}.${targetDomain}"
+    export BORG_PASSCOMMAND="${pass}/bin/pass hosts/$(hostname)/nwbackup.borgkey"
+    archiveName="$(hostname)-$(date +%Y-%m-%dT%H:%M:%S)"
+
+    ${borgbackup}/bin/borg create \
+      --verbose \
+      --filter AME \
+      --list \
+      --stats \
+      --show-rc \
+      --compression auto,lzma,6 \
+      --exclude-caches \
+      --exclude '/Users/*/.cache/*' \
+      --exclude '/Users/*/Applications/*' \
+      --exclude '/Users/*/Library/Caches/*' \
+      --exclude '/Users/*/.Trash/*' \
+      --exclude '/Users/*/.DS_Store' \
+      \
+      ::$archiveName \
+      $HOME
+
+    ${pkgs.borgbackup}/bin/borg info "::$archiveName"
+  '';
+
+  init-script = writers.writeDashBin "nwbackup-osx-${target}-init" ''
+    set -e
+
+    export BORG_REPO="borg-$(hostname -s)@${targetHost}"
+    export BORG_PASSCOMMAND="${pass}/bin/pass hosts/$(hostname -s)/nwbackup.borgkey"
+
+    ${borgbackup}/bin/borg init -e repokey-blake2
+  '';
+
 in
-writers.writeDashBin "nwbackup-osx-${target}" ''
-  set -e
-
-  export BORG_REPO="borg-$(hostname)@${target}.${targetDomain}"
-  export BORG_PASSCOMMAND="${pass}/bin/pass hosts/$(hostname)/nwbackup.borgkey"
-  archiveName="$(hostname)-$(date +%Y-%m-%dT%H:%M:%S)"
-
-  ${borgbackup}/bin/borg create \
-    --verbose \
-    --filter AME \
-    --list \
-    --stats \
-    --show-rc \
-    --compression auto,lzma,6 \
-    --exclude-caches \
-    --exclude '/Users/*/.cache/*' \
-    --exclude '/Users/*/Applications/*' \
-    --exclude '/Users/*/Library/Caches/*' \
-    --exclude '/Users/*/.Trash/*' \
-    --exclude '/Users/*/.DS_Store' \
-    \
-    ::$archiveName \
-    $HOME
-
-  ${pkgs.borgbackup}/bin/borg info "::$archiveName"
-''
+symlinkJoin rec {
+  name = "nwbackup-osx";
+  paths = [ backup-script init-script ];
+}
