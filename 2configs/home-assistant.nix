3cce4e8872c6c89c38f70323a9e7bc83d52c1d4a
Enno Lohmeier <enno@nerdworks.de>
2020-04-12 13:55:15 +0200

    hass: pymetno not yet in 20.03

--- 2configs/home-assistant.nix
+++ 2configs/home-assistant.nix
@@ -24,7 +24,17 @@ in
           }
         )
         ps.pyhomematic
-        ps.pymetno
+        (
+          ps.buildPythonPackage rec {
+            pname = "PyMetno";
+            version = "0.5.0";
+            propagatedBuildInputs = with ps; [ aiohttp xmltodict pytz ];
+            src = ps.fetchPypi {
+              inherit pname version;
+              sha256 = "0j0rl81xdmdi13krdrmzyfk5shviq8czfs1xgr0100i0jm258cp5";
+            };
+          }
+        )
         ps.pynacl
         ps.pysonos
         ps.ssdp
