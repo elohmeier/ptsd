846ec3a36e86bad5557acf0a6149d50fb92ba8ff
Enno Lohmeier <enno@nerdworks.de>
2020-04-14 10:17:45 +0200

    tp1: add printer

--- 1systems/tp1/config.nix
+++ 1systems/tp1/config.nix
@@ -32,6 +32,24 @@
       };
   };
 
+  hardware.printers = {
+    ensureDefaultPrinter = "MFC7440N";
+    ensurePrinters = [
+      {
+        name = "MFC7440N";
+        deviceUri = "socket://192.168.178.33:9100";
+        model = "drv:///brlaser.drv/br7360n.ppd";
+        ppdOptions = {
+          PageSize = "A4";
+          Resolution = "600dpi";
+          InputSlot = "Auto";
+          MediaType = "PLAIN";
+          brlaserEconomode = "False";
+        };
+      }
+    ];
+  };
+
   #boot.plymouth.enable = true;
 
   #  # https://github.com/anbox/anbox/issues/253
