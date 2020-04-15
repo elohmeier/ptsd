6d34ed7d3d355abc2236c509e7a7574fa45f6a34
Enno Lohmeier <enno@nerdworks.de>
2020-04-15 15:51:56 +0200

    drone-runner-exec: 1.0.0-beta.8 -> 1.0.0-beta.9

--- 5pkgs/drone-runner-exec/default.nix
+++ 5pkgs/drone-runner-exec/default.nix
@@ -2,15 +2,15 @@
 
 buildGoModule rec {
   pname = "drone-runner-exec";
-  version = "1.0.0-beta.8";
+  version = "1.0.0-beta.9";
   goPackagePath = "github.com/drone-runners/drone-runner-exec";
 
-  modSha256 = "0xv4fjx2a7da9mcj6ylzaj7ya6clzhv598xwafzd7l3p813rxa7j";
+  modSha256 = "068319irh0vqvly4ylbfhw6ap7i09vgsjnd17s066f4q02v29swk";
 
   src = fetchFromGitHub {
     owner = "drone-runners";
     repo = "drone-runner-exec";
     rev = "v${version}";
-    sha256 = "1dw7iaib3hq69njm8lq9xr7sj29v99jxjpbqjf0g6p6cw512llrs";
+    sha256 = "1ay4vla54hrc3ranyc6whpqh0r1nynjg6x2ab1z1259ry9xzm4xs";
   };
 }
