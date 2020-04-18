1ffa06831c68bfb9274634bda271f21e92e3359d
Enno Lohmeier <enno@nas1.host.nerdworks.de>
2020-04-18 08:43:44 +0200

    nixos-mailserver: initial at 289f71e

--- update-nixos-mailserver.sh
+++ update-nixos-mailserver.sh
@@ -3,8 +3,8 @@
 dir=$(dirname $0)
 oldrev=$(cat $dir/nixos-mailserver.json | jq -r .rev | sed 's/\(.\{7\}\).*/\1/')
 nix-prefetch-git \
-  --url https://gitlab.com/simple-nixos-mailserver/nixos-mailserver.git \
-  --rev refs/heads/v2.3.0 \
+  --url https://gitlab.com/simple-nixos-mailserver/nixos-mailserver \
+  --rev refs/tags/v2.3.0 \
 > $dir/nixos-mailserver.json
 newrev=$(cat $dir/nixos-mailserver.json | jq -r .rev | sed 's/\(.\{7\}\).*/\1/')
 git commit $dir/nixos-mailserver.json -m "nixos-mailserver: $oldrev -> $newrev"
