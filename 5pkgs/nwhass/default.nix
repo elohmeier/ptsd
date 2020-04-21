17d8d79e8ed02aef5a167963d45b914a92c17c14
Enno Lohmeier <enno@nerdworks.de>
2020-04-21 20:03:28 +0200

    nwhass: caldav support

--- 5pkgs/nwhass/default.nix
+++ 5pkgs/nwhass/default.nix
@@ -2,6 +2,20 @@
 
 home-assistant.override {
   extraPackages = ps: with ps; [
+    (
+      # waits for https://github.com/NixOS/nixpkgs/pull/85686
+      ps.caldav.overrideAttrs (
+        old: {
+          # xandikos is only a optional test dependency,
+          # not available for python3
+          postPatch = ''
+            substituteInPlace setup.py \
+              --replace ", 'xandikos'" ""
+          '';
+          meta.broken = false;
+        }
+      )
+    )
     ps.google_api_python_client # for calendar integration
     ps.hass-nabucasa # required for mobile app
     ps.influxdb
