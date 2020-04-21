38ff6dc85895bd5e249571edf04f443f0aacbf30
Enno Lohmeier <enno@nerdworks.de>
2020-04-21 20:06:07 +0200

    apu2: proxy dlrg cloud requests

--- 1systems/apu2/config.nix
+++ 1systems/apu2/config.nix
@@ -79,4 +79,68 @@ in
         then alert
     ''
   ];
+
+  services.nginx = {
+    enable = true;
+
+    commonHttpConfig = ''
+      charset UTF-8;
+    '';
+
+    virtualHosts = {
+      "192.168.168.41" = {
+        listen = [
+          {
+            addr = "192.168.168.41";
+            port = 8123;
+          }
+        ];
+
+        # proxy hass traffic
+        # hass is configured to listen on 127.0.0.1:8123
+        locations."/" = {
+          extraConfig = ''
+            proxy_pass http://127.0.0.1:8123;
+          '';
+        };
+        locations."/api/websocket" = {
+          extraConfig = ''
+            proxy_pass http://127.0.0.1:8123;
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection "Upgrade";
+            proxy_set_header Host $host;
+          '';
+        };
+
+        # proxy DLRG cloud to work around CSP iframe restrictions 
+        # for calendar embedding
+        locations."/apps" = {
+          extraConfig = ''
+            proxy_pass https://www.dlrg.cloud:443;
+          '';
+        };
+        locations."/css" = {
+          extraConfig = ''
+            proxy_pass https://www.dlrg.cloud:443;
+          '';
+        };
+        locations."/core" = {
+          extraConfig = ''
+            proxy_pass https://www.dlrg.cloud:443;
+          '';
+        };
+        locations."/js" = {
+          extraConfig = ''
+            proxy_pass https://www.dlrg.cloud:443;
+          '';
+        };
+        locations."/remote.php" = {
+          extraConfig = ''
+            proxy_pass https://www.dlrg.cloud:443;
+          '';
+        };
+      };
+    };
+  };
 }
