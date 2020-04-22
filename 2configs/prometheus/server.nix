a28a071aa3bd83748db3356b59172834e6b4f877
Enno Lohmeier <enno@nerdworks.de>
2020-04-22 11:49:26 +0200

    prometheus: add blackbox

--- 2configs/prometheus/server.nix
+++ 2configs/prometheus/server.nix
@@ -4,6 +4,23 @@ with import <ptsd/lib>;
 let
   universe = import <ptsd/2configs/universe.nix>;
   vpnNodes = netname: filterAttrs (hostname: hostcfg: hostname != config.networking.hostName && hasAttrByPath [ "nets" netname ] hostcfg) universe.hosts;
+
+  blackboxConfigJSON = pkgs.writeText "blackbox.json"
+    (
+      builtins.toJSON {
+        modules = {
+          https_2xx = {
+            prober = "http";
+            http = {
+              fail_if_not_ssl = true;
+            };
+          };
+        };
+      }
+    );
+  blackboxConfigFile = pkgs.runCommand "blackbox.yaml" { preferLocalBuild = true; } ''
+    ${pkgs.remarshal}/bin/json2yaml -i ${blackboxConfigJSON} -o $out
+  '';
 in
 {
   # access via localhost
@@ -34,6 +51,41 @@ in
           ) (vpnNodes "nwvpn")
         );
       }
+      {
+        job_name = "blackbox";
+        scrape_interval = "10s";
+        metrics_path = "/probe";
+        params.module = [ "https_2xx" ];
+        static_configs = [
+          {
+            targets = [
+              "https://grafana.services.nerdworks.de"
+              "https://hass.services.nerdworks.de"
+              "https://nextcloud.services.nerdworks.de"
+              "https://www.nerdworks.de"
+            ];
+          }
+        ];
+        relabel_configs = [
+          {
+            source_labels = [ "__address__" ];
+            target_label = "__param_target";
+          }
+          {
+            source_labels = [ "__param_target" ];
+            target_label = "instance";
+          }
+          {
+            target_label = "__address__";
+            replacement = "localhost:9115"; # blackbox exporter's address
+          }
+        ];
+      }
     ];
+
+    exporters.blackbox = {
+      enable = true;
+      configFile = blackboxConfigFile;
+    };
   };
 }
