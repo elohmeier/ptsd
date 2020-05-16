243e0f53f29f1fc0f2abbcaf3cbc309b85c0312e
Enno Lohmeier <enno@nerdworks.de>
2020-05-16 18:04:00 +0200

    mb1: add fetch-tinyscans script

--- 1systems/mb1/config.nix
+++ 1systems/mb1/config.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }:
 let
   vims = pkgs.callPackage <ptsd/2configs/vims.nix> {};
+  fetch-tinyscans = pkgs.callPackage <ptsd/5pkgs/fetch-tinyscans> {};
 in
 {
   imports = [
@@ -12,6 +13,7 @@ in
   environment.systemPackages = with pkgs; [
     nixpkgs-fmt
     vims.small
+    fetch-tinyscans
   ];
 
   ptsd.nwbackup = {
