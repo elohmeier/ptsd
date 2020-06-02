8064a3fb4abca9ef6f69797cee10cc797eaa3605
Enno Lohmeier <enno@nerdworks.de>
2020-06-02 10:11:45 +0200

    i3: use rofi combi mode

--- 3modules/home/i3.nix
+++ 3modules/home/i3.nix
@@ -71,7 +71,7 @@ in
                   "${modifier}+Return" = "exec i3-sensible-terminal";
                   "${modifier}+Shift+q" = "kill";
                   #"${modifier}+d" = "exec ${pkgs.dmenu}/bin/dmenu_run";
-                  "${modifier}+d" = "exec ${pkgs.rofi}/bin/rofi -show drun";
+                  "${modifier}+d" = "exec ${pkgs.rofi}/bin/rofi -show combi";
 
                   "${modifier}+Left" = "focus left";
                   "${modifier}+Down" = "focus down";
