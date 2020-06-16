d984fb5099f0013ee1657ffbef7b9855b620221d
Enno Lohmeier <enno@nerdworks.de>
2020-06-16 10:45:37 +0200

    use nixos-mailserver 20.03 branch

--- update-nixos-mailserver.sh
+++ update-nixos-mailserver.sh
@@ -4,7 +4,7 @@ dir=$(dirname $0)
 oldrev=$(cat $dir/nixos-mailserver.json | jq -r .rev | sed 's/\(.\{7\}\).*/\1/')
 nix-prefetch-git \
   --url https://gitlab.com/simple-nixos-mailserver/nixos-mailserver \
-  --rev refs/tags/v2.3.0 \
+  --rev refs/heads/nixos-20.03 \
 > $dir/nixos-mailserver.json
 newrev=$(cat $dir/nixos-mailserver.json | jq -r .rev | sed 's/\(.\{7\}\).*/\1/')
 git commit $dir/nixos-mailserver.json -m "nixos-mailserver: $oldrev -> $newrev"
