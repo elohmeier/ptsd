ba0875db00dc41500f11eb6847b731007bf00ada
Enno Lohmeier <enno@nerdworks.de>
2020-07-01 16:33:26 +0200

    htz1/htz2: repartition

--- 1systems/htz1/physical.nix
+++ 1systems/htz1/physical.nix
@@ -1,5 +1,8 @@
 { config, pkgs, ... }:
 
+let
+  vgPrefix = "/dev/disk/by-id/dm-name-vg";
+in
 {
   imports = [
     ./config.nix
@@ -13,19 +16,49 @@
       preLVM = true;
     };
 
-  fileSystems."/" = {
-    device = "/dev/vg/root";
-    fsType = "ext4";
-  };
+  fileSystems = {
+    "/" = {
+      device = "${vgPrefix}-root";
+      fsType = "ext4";
+    };
 
-  fileSystems."/boot" = {
-    device = "/dev/sda1";
-    fsType = "ext4";
+    "/boot" = {
+      device = "/dev/sda1";
+      fsType = "ext4";
+    };
+
+    "/home" =
+      {
+        device = "${vgPrefix}-home";
+        fsType = "ext4";
+      };
+
+    "/nix" =
+      {
+        device = "${vgPrefix}-nix";
+        fsType = "ext4";
+      };
+
+    "/var" =
+      {
+        device = "${vgPrefix}-var";
+        fsType = "ext4";
+      };
+
+    "/var/log" =
+      {
+        device = "${vgPrefix}-var--log";
+        fsType = "ext4";
+      };
+
+    "/var/src" =
+      {
+        device = "${vgPrefix}-var--src";
+        fsType = "ext4";
+      };
   };
 
   swapDevices = [
-    {
-      device = "/swapfile";
-    }
+    { device = "${vgPrefix}-swap"; }
   ];
 }
