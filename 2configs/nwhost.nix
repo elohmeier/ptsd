b0ebbad7b320f0f762164323d0fa5d26caeea404
Enno Lohmeier <enno@nerdworks.de>
2020-07-13 14:56:02 +0200

    nwhost: use security.acme by default

--- 2configs/nwhost.nix
+++ 2configs/nwhost.nix
@@ -8,10 +8,41 @@ in
     <ptsd/2configs/nwhost-mini.nix>
   ];
 
-  ptsd.lego = {
-    enable = true;
-    domain = "${config.networking.hostName}.${config.networking.domain}";
-  };
+  users.groups.certs = {};
+
+  security.acme = let
+    envFile = domain: pkgs.writeText "lego-acme-dns-${domain}.env" ''
+      ACME_DNS_STORAGE_PATH=/var/lib/acme/${domain}/acme-dns-store.json
+      ACME_DNS_API_BASE=https://auth.nerdworks.de
+    '';
+  in
+    {
+      email = "elo-lenc@nerdworks.de";
+      acceptTerms = true;
+      certs = {
+        "${config.networking.hostName}.${config.networking.domain}" = {
+          dnsProvider = "acme-dns";
+          credentialsFile = envFile "${config.networking.hostName}.${config.networking.domain}";
+          group = "certs";
+          allowKeysForGroup = true;
+          #dnsPropagationCheck = false;
+        };
+      };
+    };
+
+  ptsd.nwtraefik = let
+    hostCert = {
+      certFile = "/var/lib/acme/${config.networking.hostName}.${config.networking.domain}/cert.pem";
+      keyFile = "/var/lib/acme/${config.networking.hostName}.${config.networking.domain}/key.pem";
+    };
+  in
+    {
+      certificates = [
+        hostCert
+      ];
+      defaultCertificate = hostCert;
+      groups = "certs";
+    };
 
   ptsd.nwtelegraf.enable = true;
 
