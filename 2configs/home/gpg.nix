0239a65b850845883deaf58aea613a384c1792f4
Enno Lohmeier <enno@nerdworks.de>
2020-07-23 07:50:28 +0200

    gpg: setup forwarding

--- 2configs/home/gpg.nix
+++ 2configs/home/gpg.nix
@@ -10,11 +10,23 @@
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
+    enableExtraSocket = true;
     extraConfig = ''
       # https://github.com/drduh/config/blob/master/gpg-agent.conf
-            # https://www.gnupg.org/documentation/manuals/gnupg/Agent-Options.html
-                  default-cache-ttl 60
-                        max-cache-ttl 120
+      # https://www.gnupg.org/documentation/manuals/gnupg/Agent-Options.html
+      default-cache-ttl 60
+      max-cache-ttl 120
     '';
+  };
+
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "ws1-osx" = {
+        hostname = "192.168.178.71";
+        forwardAgent = true;
+        extraOptions.RemoteForward = "/Users/enno/.gnupg/S.gpg-agent /run/user/1000/gnupg/S.gpg-agent.extra";
+      };
+    };
   };
 }
