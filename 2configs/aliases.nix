cb3fde548f82c2dbfbc57221c51a6da80dee083b
Enno Lohmeier <enno@nerdworks.de>
2020-07-23 15:20:55 +0200

    aliases: add gapf/gaapf

--- 2configs/aliases.nix
+++ 2configs/aliases.nix
@@ -6,6 +6,8 @@
   "cd.." = "cd ..";
   gs = "git status";
   ga = "git add";
+  gapf = "git commit --amend --no-edit && git push --force";
+  gaapf = "git add . && git commit --amend --no-edit && git push --force";
   "ga." = "git add .";
   gc = "git commit";
   gco = "git checkout";
