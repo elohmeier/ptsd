707f2c76835de755537de853fc7e08a49b9fb494
Enno Lohmeier <enno@nerdworks.de>
2020-08-10 18:39:35 +0200

    qemu: fix path

--- 5pkgs/default.nix
+++ 5pkgs/default.nix
@@ -17,12 +17,12 @@ mapAttrs
   # From https://github.com/t184256/nix-on-droid/wiki/Use-a-remote-builder-with-qemu
   qemu-user-arm =
     if self.stdenv.system == "x86_64-linux"
-    then self.pkgsi686Linux.callPackage ../6cipkgs/qemu/qemu { user_arch = "arm"; }
-    else self.callPackage ../6cipkgs/qemu/qemu { user_arch = "arm"; };
-  qemu-user-x86 = self.callPackage ../6cipkgs/qemu/qemu { user_arch = "x86_64"; };
-  qemu-user-arm64 = self.callPackage ../6cipkgs/qemu/qemu { user_arch = "aarch64"; };
-  qemu-user-riscv32 = self.callPackage ../6cipkgs/qemu/qemu { user_arch = "riscv32"; };
-  qemu-user-riscv64 = self.callPackage ../6cipkgs/qemu/qemu { user_arch = "riscv64"; };
+    then self.pkgsi686Linux.callPackage ../6cipkgs/qemu { user_arch = "arm"; }
+    else self.callPackage ../6cipkgs/qemu { user_arch = "arm"; };
+  qemu-user-x86 = self.callPackage ../6cipkgs/qemu { user_arch = "x86_64"; };
+  qemu-user-arm64 = self.callPackage ../6cipkgs/qemu { user_arch = "aarch64"; };
+  qemu-user-riscv32 = self.callPackage ../6cipkgs/qemu { user_arch = "riscv32"; };
+  qemu-user-riscv64 = self.callPackage ../6cipkgs/qemu { user_arch = "riscv64"; };
 }
 # left for illustrative purposes
 #  // {
