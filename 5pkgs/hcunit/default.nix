fca286eef851f6c0b3b22df534d3739db54aa547
Enno Lohmeier <enno@nerdworks.de>
2020-08-12 12:36:20 +0200

    hcunit: init at 0.7.4

---
+++ 5pkgs/hcunit/default.nix
@@ -0,0 +1,16 @@
+{ stdenv, fetchFromGitHub, buildGoModule }:
+
+buildGoModule rec {
+  pname = "hcunit";
+  version = "0.7.4";
+  goPackagePath = "github.com/xchapter7x/${pname}";
+
+  modSha256 = "1hn679p1ai965s2jxd3k04fcq47qhpwbkkrblh60kk2w3aamiwgm";
+
+  src = fetchFromGitHub {
+    owner = "xchapter7x";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "14zawbgpknl3qbbcia5bv2nbjmr3kp2zx4hspkbagsc4nxci36n0";
+  };
+}
