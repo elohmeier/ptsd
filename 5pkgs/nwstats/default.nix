a6532cab50d5607e847837d32389aba187a313c2
Enno Lohmeier <enno@nerdworks.de>
2020-09-07 17:09:57 +0200

    nwstats: refactor

--- 5pkgs/nwstats/default.nix
+++ 5pkgs/nwstats/default.nix
@@ -5,7 +5,7 @@ buildPythonPackage rec {
   version = "1.0.1";
 
   src = fetchgit {
-    url = "https://git.nerdworks.de/nerdworks/nwstats.git";
+    url = "https://git.nerdworks.de/nerdworks/${pname}.git";
     rev = "refs/tags/${version}";
     sha256 = "0p5wv8zl669kgcc1smzfnnhl4wg74b3yp1dgh5nbpfz8a023crbr";
   };
