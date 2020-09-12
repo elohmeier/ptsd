e62b7cab5e925f7c57f96af6805e73cc58a1fb57
Enno Richter <enno@nerdworks.de>
2020-09-12 22:21:04 +0200

    fix update-nixpkgs.sh

--- update-nixpkgs.sh
+++ update-nixpkgs.sh
@@ -3,8 +3,8 @@
 dir=$(dirname $0)
 oldrev=$(cat $dir/nixpkgs.json | jq -r .rev | sed 's/\(.\{7\}\).*/\1/')
 nix-prefetch-git \
-  --url https://github.com/NixOS/nixpkgs-channels \
-  --rev refs/heads/nixos-20.09 \
+  --url https://github.com/NixOS/nixpkgs \
+  --rev refs/heads/release-20.09 \
 > $dir/nixpkgs.json
 newrev=$(cat $dir/nixpkgs.json | jq -r .rev | sed 's/\(.\{7\}\).*/\1/')
 git commit $dir/nixpkgs.json -m "nixpkgs: $oldrev -> $newrev"
