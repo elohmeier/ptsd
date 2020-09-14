de0decb83230304be90c0258d14c87fe0739380b
Enno Richter <enno@nerdworks.de>
2020-09-14 15:41:55 +0200

    vim: configure tmux integration

--- 2configs/vims.nix
+++ 2configs/vims.nix
@@ -16,25 +16,36 @@ let
       sha256 = "0gwyn9ff3f9pn5mkk31sxrs230p7fy66399p2yqy43xfqv36mzwl";
     };
   };
-  vim-tickscript = vimUtils.buildVimPluginFrom2Nix {
+  vim-tickscript = vimUtils.buildVimPluginFrom2Nix rec {
     pname = "vim-tickscript";
     version = "2017-12-01";
     src = fetchFromGitHub {
       owner = "nathanielc";
-      repo = "vim-tickscript";
+      repo = pname;
       rev = "399e332b709f034421c83af9ea14380d71e0d743";
       sha256 = "1fnv4vs3lngr3jn74p71dz5xgjlmy6qmr6xnfchx1k32bsidzjxj";
     };
   };
   tabnine-vim = vimUtils.buildVimPluginFrom2Nix rec {
     pname = "tabnine-vim";
-    version = "2.8.2";
+    version = "2.9.0";
     src = fetchFromGitHub {
       owner = "zxqfl";
       repo = pname;
       rev = version;
-      sha256 = "1rf27wf6il3gj2v05p9dg1j1p688f97zc959kf4wvf4dyxlv6v7x";
+      sha256 = "1vfh1bp8s8yrczd3lfwldf48d7yqrs1c25kkf69n991m0nk6h7kx";
     };
+  };
+  nerdtree-git-plugin = vimUtils.buildVimPluginFrom2Nix rec {
+    pname = "nerdtree-git-plugin";
+    version = "2020-09-11";
+    src = fetchFromGitHub {
+      owner = "albfan";
+      repo = pname;
+      rev = "a8c031f11dd312f53357729ca47ad493e798aa86";
+      sha256 = "1d64cmywhj43q9fkrh0kcfsxa7ijxcb1fbz38pxaacg082y6l0jy";
+    };
+    meta.homepage = "https://github.com/albfan/nerdtree-git-plugin/";
   };
   commonrc = ''
     set colorcolumn=88
@@ -117,7 +128,7 @@ in
       '';
 
       packages.myVimPackage = with vimPlugins; {
-        start = [ vim-nix vim-beancount nerdtree nerdtree-git-plugin ale vim-renamer tabnine-vim vim-tickscript vim-airline vim-airline-themes ctrlp-vim fugitive ];
+        start = [ vim-nix vim-beancount nerdtree nerdtree-git-plugin ale vim-renamer tabnine-vim vim-tickscript vim-airline vim-airline-themes ctrlp-vim fugitive vim-tmux-navigator ];
       };
     };
   };
