80c3e192436b35bc9fdf30df2f80a8ec07426368
Enno Richter <enno@nerdworks.de>
2020-09-24 22:01:33 +0200

    shell: add generic mk-dummy

--- shell.nix
+++ shell.nix
@@ -21,12 +21,14 @@ let
       ./update-nixpkgs.sh
       ./update-nixpkgs-unstable.sh
     '';
+    mk-dummy = ''
+      HOSTNAME="''${1?must provide hostname}"
+      nix-build '<nixpkgs/nixos>' -A system -I nixos-config=1systems/$HOSTNAME/physical.nix -I secrets-shared=dummy-secrets -I client-secrets=dummy-secrets -I secrets=dummy-secrets -I ptsd=$(pwd)
+    '';
     mk-drone-yml = "${pkgs.drone-cli}/bin/drone jsonnet --stream";
     mk-nwvpn-qr = "nix-build -E 'with import <nixpkgs> {}; callPackage ./5pkgs/nwvpn-qr {}'";
     mk-iso = "nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config=2configs/iso.nix -I /var/src -I ptsd=$(pwd)";
     mk-eee1 = "sudo nix-build '<nixpkgs/nixos>' -A system -I nixos-config=1systems/eee1/physical.nix -I secrets=/var/src/secrets-eee1 -I /var/src -I ptsd=$(pwd) --argstr system i686-linux";
-    mk-tp1-dummy = "nix-build '<nixpkgs/nixos>' -A system -I nixos-config=1systems/tp1/physical.nix -I secrets-shared=dummy-secrets -I client-secrets=dummy-secrets -I secrets=dummy-secrets -I ptsd=$(pwd)";
-    mk-ws1-dummy = "nix-build '<nixpkgs/nixos>' -A system -I nixos-config=1systems/ws1/physical.nix -I secrets-shared=dummy-secrets -I client-secrets=dummy-secrets -I secrets=dummy-secrets -I ptsd=$(pwd)";
   };
 in
 pkgs.mkShell {
