8375028c99dd6204016900698fb3929676b4f781
Enno Richter <enno@nerdworks.de>
2020-10-06 11:26:37 +0200

    nwhass: add missing deps

--- 5pkgs/nwhass/default.nix
+++ 5pkgs/nwhass/default.nix
@@ -2,22 +2,28 @@
 
 home-assistant.override {
   extraPackages = ps: with ps; [
+    ps.brother
     ps.caldav
-    ps.defusedxml # required for pyfritzhome
+    ps.defusedxml # required by pyfritzhome
+    ps.emoji # required by mobile_app integration
     ps.google_api_python_client # for calendar integration
-    ps.hass-nabucasa # required for mobile app
+    ps.hass-nabucasa # required by mobile app
     ps.gtts-token # for google tts
     ps.influxdb-client
     ps.influxdb
+    ps.jsonpath # required by dwd integration
     ps.mutagen # for tts
     ps.netdisco # required by pyfritzhome
     ps.paho-mqtt
     ps.pyicloud
+    ps.pyipp
     ps.psycopg2
     ps.pyfritzhome
     ps.pyhomematic
     ps.pymetno
     ps.pynacl
+    ps.pysonos
+    ps.spotipy
     ps.ssdp
     ps.zeroconf
   ];
