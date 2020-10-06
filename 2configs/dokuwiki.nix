78a54d980603d17893df650f712e2ebd71b795cc
Enno Richter <enno@nerdworks.de>
2020-10-06 21:33:56 +0200

    dokuwiki WIP

--- 2configs/dokuwiki.nix
+++ 2configs/dokuwiki.nix
@@ -10,52 +10,56 @@ in
     }
   ];
 
-  nixpkgs = {
-    config.packageOverrides = pkgs: {
-      dokuwiki = pkgs.symlinkJoin {
-        name = "dokuwiki-with-plugins";
-        paths = [
-          (
-            pkgs.dokuwiki.overrideAttrs (
-              old: rec {
-                # original preload plus DOKU_PLUGIN definition
-                preload = pkgs.writeText "preload.php" ''
-                  <?php
+  # nixpkgs = {
+  #   config.packageOverrides = pkgs: {
+  #     dokuwiki = pkgs.symlinkJoin {
+  #       name = "dokuwiki-with-plugins";
+  #       paths = [
+  #         (
+  #           pkgs.dokuwiki.overrideAttrs (
+  #             old: rec {
+  #               # original preload plus DOKU_PLUGIN definition
+  #               preload = pkgs.writeText "preload.php" ''
+  #                 <?php
 
-                    $config_cascade = array(
-                      'acl' => array(
-                        'default'   => getenv('DOKUWIKI_ACL_AUTH_CONFIG'),
-                      ),
-                      'plainauth.users' => array(
-                        'default'   => getenv('DOKUWIKI_USERS_AUTH_CONFIG'),
-                        'protected' => "" // not used by default
-                      ),
-                    );
-    
-                    define('DOKU_PLUGIN', getenv('DOKUWIKI_PLUGINS_DIR'));
-                '';
+  #                   $config_cascade = array(
+  #                     'acl' => array(
+  #                       'default'   => getenv('DOKUWIKI_ACL_AUTH_CONFIG'),
+  #                     ),
+  #                     'plainauth.users' => array(
+  #                       'default'   => getenv('DOKUWIKI_USERS_AUTH_CONFIG'),
+  #                       'protected' => "" // not used by default
+  #                     ),
+  #                   );
 
-                installPhase = ''
-                  mkdir -p $out/share/dokuwiki
-                  cp -r * $out/share/dokuwiki
-                  cp ${preload} $out/share/dokuwiki/inc/preload.php
-                  cp ${old.phpLocalConfig} $out/share/dokuwiki/conf/local.php
-                  cp ${old.phpPluginsLocalConfig} $out/share/dokuwiki/conf/plugins.local.php
-                '';
-              }
-            )
-          )
+  #                   define('DOKU_PLUGIN', getenv('DOKUWIKI_PLUGINS_DIR'));
+  #               '';
 
-          # remember to clear the cache (/var/lib/dokuwiki/data/cache) when modifying plugins
-          (pkgs.callPackage <ptsd/5pkgs/dokuwiki-plugin-dw2pdf> { })
-          (pkgs.callPackage <ptsd/5pkgs/dokuwiki-plugin-nspages> { })
-          (pkgs.callPackage <ptsd/5pkgs/dokuwiki-plugin-pagebreak> { })
-        ];
-      };
-    };
+  #               installPhase = ''
+  #                 mkdir -p $out/share/dokuwiki
+  #                 cp -r * $out/share/dokuwiki
+  #                 cp ${preload} $out/share/dokuwiki/inc/preload.php
+  #                 cp ${old.phpLocalConfig} $out/share/dokuwiki/conf/local.php
+  #                 cp ${old.phpPluginsLocalConfig} $out/share/dokuwiki/conf/plugins.local.php
+  #               '';
+  #             }
+  #           )
+  #         )
+
+  #         # remember to clear the cache (/var/lib/dokuwiki/data/cache) when modifying plugins
+  #         (pkgs.callPackage <ptsd/5pkgs/dokuwiki-plugin-dw2pdf> { })
+  #         (pkgs.callPackage <ptsd/5pkgs/dokuwiki-plugin-nspages> { })
+  #         (pkgs.callPackage <ptsd/5pkgs/dokuwiki-plugin-pagebreak> { })
+  #       ];
+  #     };
+  #   };
+  # };
+
+  ptsd.secrets.files."dokuwiki.users" = {
+    owner = "nginx";
   };
 
-  services.dokuwiki = {
+  services.dokuwiki.nwwiki = {
     enable = true;
     hostName = domain;
     nginx = {
@@ -72,7 +76,7 @@ in
     acl = ''
       * @ALL 0
     '';
-    usersFile = <secrets/dokuwiki.users>;
+    usersFile = "/run/keys/dokuwiki.users";
     superUser = "@staff";
     pluginsConfig = ''
       $plugins['authad'] = 0;
@@ -83,8 +87,13 @@ in
       $plugins['dw2pdf'] = 1;
       $plugins['nspages'] = 1;
     '';
+    plugins = with pkgs; [
+      dokuwiki-plugin-dw2pdf
+      dokuwiki-plugin-nspages
+      dokuwiki-plugin-pagebreak
+    ];
   };
-  services.phpfpm.pools.dokuwiki.phpEnv = {
-    DOKUWIKI_PLUGINS_DIR = "${pkgs.dokuwiki}/share/dokuwiki/lib/plugins/";
-  };
+  # services.phpfpm.pools.dokuwiki.phpEnv = {
+  #   DOKUWIKI_PLUGINS_DIR = "${pkgs.dokuwiki}/share/dokuwiki/lib/plugins/";
+  # };
 }
