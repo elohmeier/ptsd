10175bbe4384ea1c6b5488a513919487ed592b91
Enno Richter <enno@nerdworks.de>
2020-10-09 00:01:21 +0200

    apu1: grub serial & stateless-root wip

--- 2configs/hw/apu1.nix
+++ 2configs/hw/apu1.nix
@@ -4,9 +4,17 @@
   boot.initrd.availableKernelModules =
     [ "ahci" "ohci_pci" "ehci_pci" "usb_storage" "sd_mod" ];
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub = {
+    enable = true;
+    version = 2;
+    device = "/dev/sda";
+    extraConfig =
+      ''
+        serial --unit=0 --speed=115200 --word=8 --parity=no --stop=1
+        terminal_input --append serial
+        terminal_output --append serial
+      '';
+  };
 
   boot.kernelModules = [ "kvm-amd" ];
   boot.kernelParams = [ "console=tty0" "console=ttyS0,115200n8" ];
