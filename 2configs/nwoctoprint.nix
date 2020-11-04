ac49ee12d7bc7f3d75cdbdc85d33b82636a859ca
Enno Richter <enno@nerdworks.de>
2020-11-04 15:24:00 +0100

    nwoctoprint: use android ip cam

--- 2configs/nwoctoprint.nix
+++ 2configs/nwoctoprint.nix
@@ -16,28 +16,37 @@ in
       plugins.printtimegenius
       (plugins.callPackage ../5pkgs/octoprint-plugins/telegram.nix { })
     ];
-    webcamStreamUrl = "https://${domain}/mjpg/?action=stream";
-    webcamSnapshotUrl = "https://${domain}/mjpg/?action=snapshot";
+    #deviceService = "sys-devices-pci0000:00-0000:00:15.0-usb1-1\\x2d3-1\\x2d3:1.0-ttyUSB0-tty-ttyUSB0.device";
+    #webcamStreamUrl = "https://${domain}/mjpg/?action=stream";
+    #webcamSnapshotUrl = "https://${domain}/mjpg/?action=snapshot";
+    webcamStreamUrl = "https://${domain}/ipcam/video";
+    webcamSnapshotUrl = "https://${domain}/ipcam/shot.jpg";
   };
 
   environment.systemPackages = [ v4l-utils-nogui ];
 
-  ptsd.mjpg-streamer = {
-    enable = true;
-    inputPlugin = "input_uvc.so -f 30 -r 1280x720";
-    outputPlugin = "output_http.so -w @www@ -n -p ${toString config.ptsd.nwtraefik.ports.mjpg-streamer}";
-    deviceService = "sys-devices-pci0000:00-0000:00:15.0-usb1-1\\x2d7-1\\x2d7:1.2-sound-card1.device";
-  };
+  # ptsd.mjpg-streamer = {
+  #   enable = true;
+  #   inputPlugin = "input_uvc.so -f 30 -r 1280x720";
+  #   outputPlugin = "output_http.so -w @www@ -n -p ${toString config.ptsd.nwtraefik.ports.mjpg-streamer}";
+  #   deviceService = "sys-devices-pci0000:00-0000:00:15.0-usb1-1\\x2d7-1\\x2d7:1.2-sound-card1.device";
+  # };
 
   ptsd.nwtraefik.services = [
     {
       name = "octoprint";
       rule = "Host(`${domain}`)";
     }
+    # {
+    #   name = "mjpg-streamer";
+    #   rule = "Host(`${domain}`) && PathPrefix(`/mjpg/`)";
+    #   stripPrefixes = [ "/mjpg/" ];
+    # }
     {
-      name = "mjpg-streamer";
-      rule = "Host(`${domain}`) && PathPrefix(`/mjpg/`)";
-      stripPrefixes = [ "/mjpg/" ];
+      name = "ipcam";
+      rule = "Host(`${domain}`) && PathPrefix(`/ipcam/`)";
+      stripPrefixes = [ "/ipcam/" ];
+      url = "http://192.168.178.58:8080";
     }
   ];
 }
