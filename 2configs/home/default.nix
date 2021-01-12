b4869078f9196878747fcd320fa8636cb7e9ea54
Enno Richter <enno@nerdworks.de>
2021-01-12 11:42:17 +0100

    xsession-i3: move x11-indepedent config to default.nix

--- 2configs/home/default.nix
+++ 2configs/home/default.nix
@@ -22,4 +22,56 @@
     enable = true;
     enableZshIntegration = true;
   };
+
+  home = {
+    file.".mozilla/native-messaging-hosts/passff.json".source = "${pkgs.passff-host}/share/passff-host/passff.json";
+    keyboard = {
+      layout = "de";
+      variant = "nodeadkeys";
+    };
+    packages = with pkgs;
+      [
+        zathura
+        zathura-single
+        (makeDesktopItem {
+          name = "zathura";
+          desktopName = "Zathura";
+          exec = "${pkgs.zathura}/bin/zathura %f";
+          mimeType = "application/pdf";
+          type = "Application";
+        })
+        caffeine
+        mpv
+      ];
+  };
+
+  xdg.mimeApps = {
+    enable = true;
+
+    # verify using `xdg-mime query default <mimetype>`
+    defaultApplications = {
+      "application/pdf" = [ "zathura.desktop" ];
+      "text/plain" = [ "vim.desktop" ];
+      "image/gif" = [ "sxiv.desktop" ];
+      "image/jpeg" = [ "sxiv.desktop" ];
+      "image/png" = [ "sxiv.desktop" ];
+      "inode/directory" = [ "pcmanfm.desktop" ];
+      "text/html" = [ "chromium.desktop" "firefox.desktop" ];
+      "x-scheme-handler/http" = [ "chromium.desktop" "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "chromium.desktop" "firefox.desktop" ];
+      "x-scheme-handler/about" = [ "chromium.desktop" "firefox.desktop" ];
+      "x-scheme-handler/unknown" = [ "chromium.desktop" "firefox.desktop" ];
+      "x-scheme-handler/msteams" = [ "teams.desktop" ];
+      "application/vnd.jgraph.mxfile" = [ "drawio.desktop" ];
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = [ "writer.desktop" ];
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = [ "calc.desktop" ];
+      "application/vnd.openxmlformats-officedocument.presentationml.presentation" = [ "impress.desktop" ];
+      "application/msword" = [ "writer.desktop" ];
+      "application/msexcel" = [ "calc.desktop" ];
+      "application/mspowerpoint" = [ "impress.desktop" ];
+      "application/vnd.oasis.opendocument.text" = [ "writer.desktop" ];
+      "application/vnd.oasis.opendocument.spreadsheet" = [ "calc.desktop" ];
+      "application/vnd.oasis.opendocument.presentation" = [ "impress.desktop" ];
+    };
+  };
 }
--- 2configs/home/xsession-i3.nix
+++ 2configs/home/xsession-i3.nix
@@ -33,11 +33,6 @@ in
     };
   };
 
-  nixpkgs = {
-    config.allowUnfree = true;
-    config.packageOverrides = import ../../5pkgs pkgs;
-  };
-
   ptsd.i3 = {
     enable = true;
     screenshotCommand = "exec ${pkgs.flameshot}/bin/flameshot gui";
@@ -50,58 +45,9 @@ in
 
   ptsd.pcmanfm.enable = true;
 
-  home = {
-    file.".mozilla/native-messaging-hosts/passff.json".source = "${pkgs.passff-host}/share/passff-host/passff.json";
-    keyboard = {
-      layout = "de";
-      variant = "nodeadkeys";
-    };
-    packages = with pkgs;
-      [
-        xorg.xev
-        xorg.xhost
-        zathura
-        zathura-single
-        (makeDesktopItem {
-          name = "zathura";
-          desktopName = "Zathura";
-          exec = "${pkgs.zathura}/bin/zathura %f";
-          mimeType = "application/pdf";
-          type = "Application";
-        })
-        caffeine
-        mpv
-        flameshot
-      ];
-  };
-
-  xdg.mimeApps = {
-    enable = true;
-
-    # verify using `xdg-mime query default <mimetype>`
-    defaultApplications = {
-      "application/pdf" = [ "zathura.desktop" ];
-      "text/plain" = [ "vim.desktop" ];
-      "image/gif" = [ "sxiv.desktop" ];
-      "image/jpeg" = [ "sxiv.desktop" ];
-      "image/png" = [ "sxiv.desktop" ];
-      "inode/directory" = [ "pcmanfm.desktop" ];
-      "text/html" = [ "chromium.desktop" "firefox.desktop" ];
-      "x-scheme-handler/http" = [ "chromium.desktop" "firefox.desktop" ];
-      "x-scheme-handler/https" = [ "chromium.desktop" "firefox.desktop" ];
-      "x-scheme-handler/about" = [ "chromium.desktop" "firefox.desktop" ];
-      "x-scheme-handler/unknown" = [ "chromium.desktop" "firefox.desktop" ];
-      "x-scheme-handler/msteams" = [ "teams.desktop" ];
-      "application/vnd.jgraph.mxfile" = [ "drawio.desktop" ];
-      "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = [ "writer.desktop" ];
-      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = [ "calc.desktop" ];
-      "application/vnd.openxmlformats-officedocument.presentationml.presentation" = [ "impress.desktop" ];
-      "application/msword" = [ "writer.desktop" ];
-      "application/msexcel" = [ "calc.desktop" ];
-      "application/mspowerpoint" = [ "impress.desktop" ];
-      "application/vnd.oasis.opendocument.text" = [ "writer.desktop" ];
-      "application/vnd.oasis.opendocument.spreadsheet" = [ "calc.desktop" ];
-      "application/vnd.oasis.opendocument.presentation" = [ "impress.desktop" ];
-    };
-  };
+  home.packages = with pkgs;[
+    xorg.xev
+    xorg.xhost
+    flameshot
+  ];
 }
