e790efa739c8712b3a8f673a48c85b04241e3d4a
Enno Richter <enno@nerdworks.de>
2021-01-18 13:36:49 +0100

    prometheus: add inode-free alert

--- 2configs/prometheus/server.nix
+++ 2configs/prometheus/server.nix
@@ -363,6 +363,17 @@ in
               };
             }
             {
+              alert = "DiskInodes10%Free";
+              expr = ''node_filesystem_files_free/node_filesystem_files * 100 < 10'';
+              for = "30m";
+              labels.severity = "warning";
+              annotations = {
+                summary = "{{ $labels.alias }} disk {{ $labels.mountpoint }} inodes exhausted";
+                url = "https://grafana.services.nerdworks.de/d/hb7fSE0Zz/1-node-exporter-for-prometheus-dashboard-en-v20191102?orgId=1&var-hostname={{ $labels.alias }}";
+                description = ''The disk {{ $labels.mountpoint }} of host {{ $labels.alias }} has {{ $value | printf "%.1f" }}% free inodes remaining.'';
+              };
+            }
+            {
               alert = "EndpointDown";
               expr = "probe_success == 0";
               for = "30s";
