8b81710bf979758d16d40816f46f50eca9552b69
Enno Richter <enno@nerdworks.de>
2021-03-18 22:52:55 +0100

    htz2: integrate acme-dns

--- 1systems/htz2/config.nix
+++ 1systems/htz2/config.nix
@@ -1,13 +1,13 @@
 { config, lib, pkgs, ... }:
 let
   universe = import <ptsd/2configs/universe.nix>;
+  nets = universe.hosts."${config.networking.hostName}".nets;
 in
 {
   imports =
     [
       <ptsd>
       <ptsd/2configs>
-      <ptsd/2configs/acme-dns.nix>
       <ptsd/2configs/hardened.nix>
       <ptsd/2configs/matrix.nix>
       <ptsd/2configs/nwhost.nix>
@@ -27,8 +27,15 @@ in
       };
     };
 
-    # reduce noise coming from www if
-    firewall.logRefusedConnections = false;
+    firewall = {
+      # reduce noise coming from www if
+      logRefusedConnections = false;
+
+      interfaces.ens3 = {
+        allowedTCPPorts = [ 53 ]; # acme-dns
+        allowedUDPPorts = [ 53 ]; # acme-dns
+      };
+    };
   };
 
   # prevents creation of the following route (`ip -6 route`):
@@ -41,44 +48,27 @@ in
     enable = true;
     entryPoints = {
       "www4-http" = {
-        address = "${universe.hosts."${config.networking.hostName}".nets.www.ip4.addr}:80";
-        http.redirections.entryPoint = {
-          to = "www4-https";
-          scheme = "https";
-          permanent = true;
-        };
+        address = "${nets.www.ip4.addr}:80";
       };
       "www4-https" = {
-        address = "${universe.hosts."${config.networking.hostName}".nets.www.ip4.addr}:443";
+        address = "${nets.www.ip4.addr}:443";
       };
       "www6-http" = {
-        address = "[${universe.hosts."${config.networking.hostName}".nets.www.ip6.addr}]:80";
-        http.redirections.entryPoint = {
-          to = "www6-https";
-          scheme = "https";
-          permanent = true;
-        };
+        address = "[${nets.www.ip6.addr}]:80";
       };
       "www6-https" = {
-        address = "[${universe.hosts."${config.networking.hostName}".nets.www.ip6.addr}]:443";
+        address = "[${nets.www.ip6.addr}]:443";
       };
       "nwvpn-http" = {
-        address = "${universe.hosts."${config.networking.hostName}".nets.nwvpn.ip4.addr}:80";
-        http.redirections.entryPoint = {
-          to = "nwvpn-https";
-          scheme = "https";
-          permanent = true;
-        };
+        address = "${nets.nwvpn.ip4.addr}:80";
       };
       "nwvpn-https" = {
-        address = "${universe.hosts."${config.networking.hostName}".nets.nwvpn.ip4.addr}:443";
+        address = "${nets.nwvpn.ip4.addr}:443";
       };
 
       # added for local tls monitoring
       "loopback4-https".address = "127.0.0.1:443";
     };
-    acmeEnabled = true;
-    acmeEntryPoint = "www4-http";
     certificates =
       let
         crt = domain: {
@@ -87,8 +77,17 @@ in
         };
       in
       [
+        (crt "auth.nerdworks.de")
         (crt "mail.nerdworks.de")
+        (crt "matrix.nerdworks.de")
       ];
+    services = [
+      {
+        name = "acme-dns";
+        rule = "Host(`auth.nerdworks.de`)";
+        entryPoints = [ "www4-http" "www4-https" "www6-http" "www6-https" ];
+      }
+    ];
   };
 
   security.acme.certs =
@@ -99,11 +98,75 @@ in
       '';
     in
     {
+      "auth.nerdworks.de" = {
+        webroot = config.ptsd.nwacme.http.webroot;
+        credentialsFile = envFile "auth.nerdworks.de";
+        group = "certs";
+        postRun = "systemctl restart traefik.service";
+      };
+
       "mail.nerdworks.de" = {
-        dnsProvider = "acme-dns";
+        webroot = config.ptsd.nwacme.http.webroot;
         credentialsFile = envFile "mail.nerdworks.de";
         group = "certs";
         postRun = "systemctl restart traefik.service";
       };
+
+      "matrix.nerdworks.de" = {
+        webroot = config.ptsd.nwacme.http.webroot;
+        credentialsFile = envFile "matrix.nerdworks.de";
+        group = "certs";
+        postRun = "systemctl restart traefik.service";
+      };
+    };
+
+  ptsd.nwacme = {
+    enable = true;
+    http.enable = true;
+    hostCert = {
+      enable = true;
+      useHTTP = true;
+    };
+  };
+
+  ptsd.acme-dns =
+    let
+      domain = "auth.nerdworks.de";
+    in
+    {
+
+      enable = true;
+
+      domain = "acme.nerdworks.de";
+      nsname = domain;
+      nsadmin = "elo-acme.nerdworks.de";
+
+      records = [
+        "${domain}. A ${nets.www.ip4.addr}"
+        "acme.nerdworks.de. NS ${domain}"
+      ];
+
+      # use only ipv4 until
+      # https://github.com/joohoi/acme-dns/issues/135 is fixed
+      # TODO: route through traefik udp/tcp
+      generalOptions = {
+        listen = "${nets.www.ip4.addr}:53";
+        protocol = "both4";
+      };
+
+      apiOptions = {
+        api_domain = domain;
+        disable_registration = false;
+        acme_cache_dir = "/var/lib/acme-dns/api-certs";
+        corsorigins = [
+          "*"
+        ];
+        use_header = true;
+        header_name = "X-Forwarded-For";
+        ip = "127.0.0.1";
+        port = toString config.ptsd.nwtraefik.ports.acme-dns;
+        tls = "none";
+      };
+
     };
 }
