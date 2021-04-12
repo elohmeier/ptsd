0004098ee1d074560f7d584cb14cce2796af2be0
Enno Richter <enno@nerdworks.de>
2021-04-12 15:53:37 +0200

    ws2: display cfg

--- 1systems/ws2/home.nix
+++ 1systems/ws2/home.nix
@@ -8,9 +8,13 @@
   ];
 
   wayland.windowManager.sway = {
+   #  extraConfig = ''
+   #    output DP-1 pos 0 0 mode 2560x1440@74.924004Hz scale 1
+   #    output HDMI-A-2 pos 2560 0 mode 2560x1440@69.928001Hz scale 1
+   #  '';
     extraConfig = ''
-      output DP-1 pos 0 0 mode 2560x1440@74.924004Hz scale 1
-      output HDMI-A-2 pos 2560 0 mode 2560x1440@69.928001Hz scale 1
+      output DP-1 pos 0 0 mode 2560x1440@74.924004Hz scale 1.5
+      output HDMI-A-2 pos 1706 0 mode 2560x1440@69.928001Hz scale 1.5
     '';
   };
 }
