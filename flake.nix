acfd9c8157b35f022d1f0ebc94ca446265889f0a
Enno Richter <enno@nerdworks.de>
2021-04-21 10:58:19 +0200

    ci: build ffmpeg

--- flake.nix
+++ flake.nix
@@ -6,43 +6,57 @@
     home-manager.url = github:nix-community/home-manager/master;
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = github:NixOS/nixos-hardware/master;
+    flake-utils.url = github:numtide/flake-utils;
   };
 
-  outputs = { self, nixpkgs, home-manager, nixos-hardware, ... }: {
+  outputs = { self, nixpkgs, home-manager, nixos-hardware, flake-utils, ... }:
 
-    packages.x86_64-linux.cachix = nixpkgs.legacyPackages.x86_64-linux.cachix;
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            config.allowUnfree = true; inherit system;
+          };
+        in
+        {
+          packages = flake-utils.lib.flattenTree {
+            cachix = pkgs.cachix;
+            ffmpeg =
+              pkgs.ffmpeg-full.override {
+                nonfreeLicensing = true;
+                fdkaacExtlib = true;
+                qtFaststartProgram = false;
+              }
+            ;
+          };
+        })
+    // {
 
-    # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      nixosConfigurations = {
+        eee1 = nixpkgs.lib.nixosSystem {
+          system = "i686-linux";
+          modules = [
+            ./1systems/eee1/physical.nix
+            home-manager.nixosModule
+          ];
+        };
 
-    # defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+        tp1 = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./1systems/tp1/physical.nix
+            home-manager.nixosModule
+            nixos-hardware.nixosModules.lenovo-thinkpad-x280
+          ];
+        };
 
-
-    nixosConfigurations = {
-      eee1 = nixpkgs.lib.nixosSystem {
-        system = "i686-linux";
-        modules = [
-          ./1systems/eee1/physical.nix
-          home-manager.nixosModule
-        ];
-      };
-
-      tp1 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./1systems/tp1/physical.nix
-          home-manager.nixosModule
-          nixos-hardware.nixosModules.lenovo-thinkpad-x280
-        ];
-      };
-
-      ws2 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./1systems/ws2/physical.nix
-          home-manager.nixosModules.home-manager
-        ];
+        ws2 = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./1systems/ws2/physical.nix
+            home-manager.nixosModules.home-manager
+          ];
+        };
       };
     };
-
-  };
 }
