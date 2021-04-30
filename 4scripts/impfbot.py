b42b9f03921318c4dc85bf4a1aea652a6c299aed
Enno Richter <enno@nerdworks.de>
2021-04-30 21:26:44 +0200

    impfbot: split commands

--- 4scripts/impfbot.py
+++ 4scripts/impfbot.py
@@ -25,37 +25,57 @@ TERMINE = []
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
 def start(update: Update, _: CallbackContext) -> None:
-    update.message.reply_text("Hi! Use /set <seconds> to set a timer")
+    update.message.reply_text(
+        """
+Hi! Folgende Kommandos sind möglich:
+
+-- Suche nach Vermittlungscode --
+/set_impfcode <seconds> <plz>
+/unset_impfcode
+
+-- Suche nach Termin mit vorhandenem Vermittlungscode --
+/set_termin <seconds> <plz> <vermittlungscode>
+/unset_impfcode
+
+<seconds> z.B. auf 800 einstellen und für die 
+<plz> im Webbrowser schauen, welche benutzt wird für das Impfzentrum.
+        """
+    )
 
 
-def impfcode_alarm(context: CallbackContext) -> None:
-    """Send the alarm message."""
-    job = context.job
+def impfcode_alarm_factory(plz: str):
+    def alarm(context: CallbackContext) -> None:
+        """ sucht ohne Vermittlungscode nach Terminen, damit man einen Vermittlungscode bekommen kann """
+        job = context.job
 
-    try:
-        res = requests.get(
-            "https://353-iz.impfterminservice.de/rest/suche/termincheck?plz=20357&leistungsmerkmale=L920,L921,L922",
-            headers={
-                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36",
-            },
-            timeout=3,
-        )
-
-        if res.status_code != 429:  # usual response: too many requests
-            context.bot.send_message(
-                job.context, text=f"{res.status_code}: {res.json()}"
+        try:
+            res = requests.get(
+                f"https://353-iz.impfterminservice.de/rest/suche/termincheck?plz={plz}&leistungsmerkmale=L920,L921,L922",
+                headers={
+                    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36",
+                },
+                timeout=3,
             )
-    except Exception as ex:
-        context.bot.send_message(job.context, text=f"alarm failed: {ex}")
+
+            if res.status_code != 429:  # usual response: too many requests
+                context.bot.send_message(
+                    job.context, text=f"{res.status_code}: {res.json()}"
+                )
+        except Exception as ex:
+            context.bot.send_message(job.context, text=f"impfcode-alarm failed: {ex}")
+
+    return alarm
 
 
-def termin_alarm_factory(impfcode: str):
+def termin_alarm_factory(impfcode: str, plz: str):
+    """ Sucht zu einem vorhandenen Vermittlungscode Termine """
+
     def alarm(context: CallbackContext) -> None:
         job = context.job
 
         try:
             res = requests.get(
-                "https://353-iz.impfterminservice.de/rest/suche/impfterminsuche?plz=20357",
+                f"https://353-iz.impfterminservice.de/rest/suche/impfterminsuche?plz={plz}",
                 headers={
                     "Authorization": "Basic %s"
                     % base64.encodebytes(b":%s" % impfcode.encode()).decode().strip(),
@@ -78,22 +98,13 @@ def termin_alarm_factory(impfcode: str):
                             job.context,
                             text=f"Neues Terminpaar: {t[0]:%d.%m.%Y %H:%M} und {t[1]:%d.%m.%Y %H:%M}",
                         )
-
-                # nothing_available = {
-                #     "gesuchteLeistungsmerkmale": ["L920", "L921"],
-                #     "termine": [],
-                #     "termineTSS": [],
-                #     "praxen": {},
-                # }
-                # if res.json() != nothing_available:
-                #     context.bot.send_message(job.context, text=res.json())
             else:
                 context.bot.send_message(
                     job.context, text=f"{res.status_code}: {res.content}"
                 )
 
         except Exception as ex:
-            context.bot.send_message(job.context, text=f"alarm failed: {ex}")
+            context.bot.send_message(job.context, text=f"termin-alarm failed: {ex}")
 
     return alarm
 
@@ -108,7 +119,7 @@ def remove_job_if_exists(name: str, context: CallbackContext) -> bool:
     return True
 
 
-def set_timer(update: Update, context: CallbackContext) -> None:
+def set_impfcode_timer(update: Update, context: CallbackContext) -> None:
     """Add a job to the queue."""
     if update.message is None:
         return
@@ -117,7 +128,52 @@ def set_timer(update: Update, context: CallbackContext) -> None:
     try:
         # args[0] should contain the time for the timer in seconds
         interval = int(context.args[0])
-        impfcode = context.args[1]
+        plz = context.args[1]
+        if interval < 0:
+            update.message.reply_text("Sorry we can not go back to future!")
+            return
+
+        job_removed = remove_job_if_exists(f"{chat_id}-impfcode", context)
+        context.job_queue.run_repeating(
+            impfcode_alarm_factory(plz),
+            interval=interval,
+            first=3,
+            context=chat_id,
+            name=f"{chat_id}-impfcode",
+        )
+
+        text = "impfcode timer successfully set!"
+        if job_removed:
+            text += " Old one was removed."
+        update.message.reply_text(text)
+
+    except (IndexError, ValueError):
+        update.message.reply_text("Usage: /set_impfcode <seconds> <plz>")
+
+
+def unset_impfcode(update: Update, context: CallbackContext) -> None:
+    """Remove the job if the user changed their mind."""
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(f"{chat_id}-impfcode", context)
+    text = (
+        "impfcode timer successfully cancelled!"
+        if job_removed
+        else "You have no active timer."
+    )
+    update.message.reply_text(text)
+
+
+def set_termin_timer(update: Update, context: CallbackContext) -> None:
+    """Add a job to the queue."""
+    if update.message is None:
+        return
+
+    chat_id = update.message.chat_id
+    try:
+        # args[0] should contain the time for the timer in seconds
+        interval = int(context.args[0])
+        plz = context.args[1]
+        impfcode = context.args[2]
         if interval < 0:
             update.message.reply_text("Sorry we can not go back to future!")
             return
@@ -126,30 +182,34 @@ def set_timer(update: Update, context: CallbackContext) -> None:
             update.message.reply_text("Sorry, invalid impfcode!")
             return
 
-        job_removed = remove_job_if_exists(str(chat_id), context)
+        job_removed = remove_job_if_exists(f"{chat_id}-termin", context)
         context.job_queue.run_repeating(
-            termin_alarm_factory(impfcode),
+            termin_alarm_factory(impfcode, plz),
             interval=interval,
             first=3,
             context=chat_id,
-            name=str(chat_id),
+            name=f"{chat_id}-termin",
         )
 
-        text = "Timer successfully set!"
+        text = "termin timer successfully set!"
         if job_removed:
             text += " Old one was removed."
         update.message.reply_text(text)
 
     except (IndexError, ValueError):
-        update.message.reply_text("Usage: /set <seconds>")
+        update.message.reply_text(
+            "Usage: /set_termin <seconds> <plz> <vermittlungscode>"
+        )
 
 
-def unset(update: Update, context: CallbackContext) -> None:
+def unset_termin(update: Update, context: CallbackContext) -> None:
     """Remove the job if the user changed their mind."""
     chat_id = update.message.chat_id
-    job_removed = remove_job_if_exists(str(chat_id), context)
+    job_removed = remove_job_if_exists(f"{chat_id}-termin", context)
     text = (
-        "Timer successfully cancelled!" if job_removed else "You have no active timer."
+        "termin timer successfully cancelled!"
+        if job_removed
+        else "You have no active timer."
     )
     update.message.reply_text(text)
 
@@ -170,8 +230,10 @@ def main() -> None:
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", start))
-    dispatcher.add_handler(CommandHandler("set", set_timer))
-    dispatcher.add_handler(CommandHandler("unset", unset))
+    dispatcher.add_handler(CommandHandler("set_impfcode", set_impfcode_timer))
+    dispatcher.add_handler(CommandHandler("unset_impfcode", unset_impfcode))
+    dispatcher.add_handler(CommandHandler("set_termin", set_termin_timer))
+    dispatcher.add_handler(CommandHandler("unset_termin", unset_termin))
 
     # Start the Bot
     updater.start_polling()
