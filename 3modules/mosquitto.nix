093ae1837e6346322b651e1e34b65f43b494b4f9
Enno Richter <enno@nerdworks.de>
2021-05-24 20:23:25 +0200

    mosquitto: rm packageOverride

--- 3modules/mosquitto.nix
+++ 3modules/mosquitto.nix
@@ -95,20 +95,6 @@ in
 
   config = mkIf cfg.enable {
 
-    # workaround https://github.com/eclipse/mosquitto/issues/1999
-    # TODO: remove in 21.03/21.05?
-    nixpkgs.config.packageOverrides = pkgs: {
-      mosquitto = pkgs.mosquitto.overrideAttrs (oldAttrs: rec {
-        version = "2.0.7";
-        src = pkgs.fetchFromGitHub {
-          owner = "eclipse";
-          repo = "mosquitto";
-          rev = "v${version}";
-          sha256 = "0i2i2743cf7bahdx8sga88cqnys29k08b29zl3v9qlc0yqnajq9x";
-        };
-      });
-    };
-
     # generate passwd file using e.g. `nix-shell -p mosquitto --run "mosquitto_passwd -c -b pw tasmota $(pass mosquitto/dlrg/tasmota)"`
     ptsd.secrets.files."mosquitto.passwd" = { };
 
