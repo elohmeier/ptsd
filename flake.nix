cedecfc1438ea78720fe7244e6b1b9d0243aa89b
Enno Richter <enno@nerdworks.de>
2021-05-26 13:30:03 +0200

    flake: switch to nixos-unstable-small

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "ptsd";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable-small;
     home-manager.url = github:nix-community/home-manager/master;
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = github:NixOS/nixos-hardware/master;
