4fa7ddd25fd89482bfac263eba36ea4a4302e27f
Enno Richter <enno@nerdworks.de>
2021-06-01 09:52:00 +0200

    hw/ws2019: fix wifi driver race condition

--- 2configs/hw/ws2019.nix
+++ 2configs/hw/ws2019.nix
@@ -4,6 +4,7 @@
   # See https://wiki.archlinux.org/index.php/Broadcom_wireless#No_5GHz_for_BCM4360_(14e4:43a0)_/_BCM43602_(14e4:43ba)_devices
 
   boot = {
+    blacklistedKernelModules = [ "b43" "bcma" ]; # prevent loading of conflicting wifi module, "wl" should be used instead
     extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
     initrd = {
       availableKernelModules = [
