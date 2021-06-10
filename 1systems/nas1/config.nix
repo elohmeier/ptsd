2960459c5f35c90f25ded7955831d0f8401909aa
Enno Richter <enno@nerdworks.de>
2021-06-10 15:17:08 +0200

    nas1: cfg

--- 1systems/nas1/config.nix
+++ 1systems/nas1/config.nix
@@ -7,7 +7,7 @@ in
   imports = [
     ../..
     ../../2configs
-    ../../2configs/cli-tools.nix
+    #../../2configs/cli-tools.nix
     #../../2configs/drone-exec-container.nix
     ../../2configs/grafana.nix
     ../../2configs/hardened.nix
