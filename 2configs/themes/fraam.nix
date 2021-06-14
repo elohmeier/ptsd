73a6f757fe97dd1e99ccbbb5bf234c71ddf90966
Enno Richter <enno@nerdworks.de>
2021-06-14 22:57:36 +0200

    fraam: moped bg

--- 2configs/themes/fraam.nix
+++ 2configs/themes/fraam.nix
@@ -2,7 +2,8 @@
 
 {
   ptsd.desktop = {
-    backgroundImage = "${pkgs.nerdworks-artwork}/scaled/wallpaper-fraam-2021-dark.png";
+    #backgroundImage = "${pkgs.nerdworks-artwork}/scaled/wallpaper-fraam-2021-dark.png";
+    backgroundImage = "/home/enno/Pocket/P1080645.jpg";
     lockImage = "${pkgs.nerdworks-artwork}/fraam_weiss.png";
     userImage = "${pkgs.nerdworks-artwork}/fraam_steuerrad_500.png";
     backgroundFill = "#193657";
