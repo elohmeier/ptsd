0c3a993e9ae305fa010932b881871bb2c7a66530
Enno Richter <enno@nerdworks.de>
2021-06-21 14:50:02 +0200

    ws2: add WIP wifi initrd code

--- 1systems/ws2/physical.nix
+++ 1systems/ws2/physical.nix
@@ -1,3 +1,5 @@
+{ pkgs, lib, ... }:
+
 {
   imports = [
     ./config.nix
@@ -10,10 +12,80 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.initrd.luks.devices = {
-    cryptlvm = {
-      device = "/dev/disk/by-id/nvme-Samsung_SSD_970_EVO_Plus_1TB_S4EWNMFN904187J-part2";
+  boot.initrd = {
+    luks.devices = {
+      cryptlvm = {
+        device = "/dev/disk/by-id/nvme-Samsung_SSD_970_EVO_Plus_1TB_S4EWNMFN904187J-part2";
+      };
     };
+
+    # availableKernelModules = [ "iwlmvm" "mac80211" "iwlwifi" "cfg80211" "af_packet" ];
+
+    # extraUtilsCommands =
+    #   ''
+    #     copy_bin_and_libs ${pkgs.wpa_supplicant}/bin/wpa_cli
+    #     copy_bin_and_libs ${pkgs.wpa_supplicant}/bin/wpa_supplicant
+    #   '';
+
+    # preLVMCommands = lib.mkBefore (
+    #   let
+    #     udhcpcScript = pkgs.writeScript "udhcp-script"
+    #       ''
+    #         #! /bin/sh
+    #         if [ "$1" = bound ]; then
+    #           ip address add "$ip/$mask" dev "$interface"
+    #           if [ -n "$mtu" ]; then
+    #             ip link set mtu "$mtu" dev "$interface"
+    #           fi
+    #           if [ -n "$staticroutes" ]; then
+    #             echo "$staticroutes" \
+    #               | sed -r "s@(\S+) (\S+)@ ip route add \"\1\" via \"\2\" dev \"$interface\" ; @g" \
+    #               | sed -r "s@ via \"0\.0\.0\.0\"@@g" \
+    #               | /bin/sh
+    #           fi
+    #           if [ -n "$router" ]; then
+    #             ip route add "$router" dev "$interface" # just in case if "$router" is not within "$ip/$mask" (e.g. Hetzner Cloud)
+    #             ip route add default via "$router" dev "$interface"
+    #           fi
+    #           if [ -n "$dns" ]; then
+    #             rm -f /etc/resolv.conf
+    #             for server in $dns; do
+    #               echo "nameserver $server" >> /etc/resolv.conf
+    #             done
+    #           fi
+    #         fi
+    #       '';
+    #     wpaCfg = pkgs.writeText "wpa_supplicant.conf" ''
+    #       network={
+    #         ssid="XXX"
+    #         psk="XXX"
+    #       }
+    #     '';
+    #   in
+    #   ''
+    #     echo "******************* START WIFI *********************"
+
+    #     echo "bringing up network interface wlan0..."
+    #     ip link set wlan0 up
+
+    #     echo "associating with AP..."
+    #     wpa_supplicant -i wlan0 -c ${wpaCfg}
+
+    #     echo "waiting 10 secs"
+    #     sleep 10
+        
+    #     echo "acquiring IP address via DHCP on wlan0..."
+    #     udhcpc --quit --now -i wlan0 -O staticroutes --script ${udhcpcScript}
+
+    #     echo "******************** END WIFI **********************"
+    #   ''
+    # );
+
+    # postMountCommands = ''
+    #   ip address flush wlan0
+    #   wpa_cli terminate -i wlan0
+    #   ip link set wlan0 down
+    # '';
   };
 
   fileSystems."/" = {
