a3dd96a0b6a441fc8b1204a9d354f7ecfa847f4d
Enno Richter <enno@nerdworks.de>
2021-07-05 12:38:24 +0200

    ws2: scale cfg

--- 1systems/ws2/home.nix
+++ 1systems/ws2/home.nix
@@ -1,4 +1,9 @@
 { config, lib, pkgs, ... }:
+
+let
+  # nix has no float2int conversion
+  roundDown = f: lib.toInt (builtins.elemAt (lib.splitString "." (toString f)) 0);
+in
 {
   imports = [
     ../../2configs/home
@@ -11,9 +16,10 @@
     #    output DP-1 pos 0 0 mode 2560x1440@74.924004Hz scale 1
     #    output HDMI-A-2 pos 2560 0 mode 2560x1440@69.928001Hz scale 1
     #  '';
-    extraConfig = ''
-      output DP-1 pos 0 0 mode 2560x1440@74.924004Hz scale 1.5
-      output HDMI-A-2 pos 1706 0 mode 2560x1440@69.928001Hz scale 1.5
-    '';
+    extraConfig = let scale = 1.25; in
+      ''
+        output DP-1 pos 0 0 mode 2560x1440@74.924004Hz scale ${toString scale}
+        output HDMI-A-2 pos ${toString (roundDown (2560 / scale))} 0 mode 2560x1440@69.928001Hz scale ${toString scale}
+      '';
   };
 }
