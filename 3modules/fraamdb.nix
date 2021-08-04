1260a84eeb42d7a3e394baa5a4f8829c076513c8
Enno Richter <enno@nerdworks.de>
2021-08-04 11:07:12 +0200

    fraamdb: disable mondayjob

--- 3modules/fraamdb.nix
+++ 3modules/fraamdb.nix
@@ -114,8 +114,6 @@ in
         ${pyenv}/bin/manage.py importtimesheets 3
         ${pyenv}/bin/manage.py updatebalances
         ${pyenv}/bin/manage.py updatepnsquotas
-
-        ${pyenv}/bin/manage.py mondayarchive
       '';
 
       serviceConfig = {
