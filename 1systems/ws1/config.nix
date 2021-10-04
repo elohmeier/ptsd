c1c20550f197c2402cba20c07a8d13186b0edb08
Enno Richter <enno@nerdworks.de>
2021-10-04 21:17:54 +0200

    ws1: use latest kernel

--- 1systems/ws1/config.nix
+++ 1systems/ws1/config.nix
@@ -54,7 +54,7 @@ in
     autostart = false;
   };
 
-  boot.kernelPackages = pkgs.linuxPackages_5_13; # nvidia_x11 not backported for >=5.14 (https://github.com/NixOS/nixpkgs/pull/136205)
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   boot.kernel.sysctl."kernel.sysrq" = 1; # allow all SysRq key combinations
 
