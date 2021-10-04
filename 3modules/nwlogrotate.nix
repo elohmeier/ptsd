a69a3db5ec4508218ab2c469e09b65abe3edfb7d
Enno Richter <enno@nerdworks.de>
2021-10-04 21:18:31 +0200

    nwlogrotate: avoid collisions with stock logrotate systemd unit

--- 3modules/nwlogrotate.nix
+++ 3modules/nwlogrotate.nix
@@ -16,19 +16,19 @@ in
 
   config = mkIf (cfg.config != "") {
 
-    systemd.services.logrotate = {
+    systemd.services.nwlogrotate = {
       description = "logrotate service";
       wantedBy = [ "multi-user.target" ];
       startAt = "daily";
 
       serviceConfig = {
-        ExecStart = "${pkgs.logrotate}/sbin/logrotate --state /var/lib/logrotate/state ${pkgs.writeText "logrotate.conf" cfg.config}";
+        ExecStart = "${pkgs.logrotate}/sbin/logrotate --state /var/lib/nwlogrotate/state ${pkgs.writeText "nwlogrotate.conf" cfg.config}";
         NoNewPrivileges = true;
         PrivateTmp = true;
         PrivateDevices = true;
         ProtectHome = true;
         ProtectSystem = "full";
-        StateDirectory = "logrotate";
+        StateDirectory = "nwlogrotate";
         Restart = "no";
       };
     };
