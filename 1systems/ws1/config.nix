3fd59b18e39efe68c8982b41b01b78d44d776000
Enno Richter <enno@nerdworks.de>
2021-11-18 11:02:42 +0100

    ws1: update font config

--- 1systems/ws1/config.nix
+++ 1systems/ws1/config.nix
@@ -83,16 +83,6 @@ in
         output "Dell Inc. DELL P2415Q D8VXF96K09HB" pos 0 2304 mode 3840x2160@59.997Hz scale 1
         output "Dell Inc. DELL P2415Q D8VXF64G0LGL" pos 3840 2304 mode 3840x2160@59.997Hz scale 1
       '';
-      #extraConfig = ''
-      #  output "Goldstar Company Ltd LG UltraFine 701NTAB7S144" pos 0 0 mode 4096x2304@59.999Hz scale 2
-      #  output "Dell Inc. DELL P2415Q D8VXF96K09HB" pos 0 1152 mode 3840x2160@59.997Hz scale 2
-      #  output "Dell Inc. DELL P2415Q D8VXF64G0LGL" pos 1920 1152 mode 3840x2160@59.997Hz scale 2
-      #'';
-      # extraConfig = ''
-      #  output "Goldstar Company Ltd LG UltraFine 701NTAB7S144" pos 0 0 mode 4096x2304@59.999Hz scale 2.191406
-      #  output "Dell Inc. DELL P2415Q D8VXF96K09HB" pos 0 1052 mode 3840x2160@59.997Hz scale 1.859375
-      #  output "Dell Inc. DELL P2415Q D8VXF64G0LGL" pos 2064 1052 mode 3840x2160@59.997Hz scale 1.859375
-      #'';
     };
     ptsd.pcmanfm.enableRdpAssistant = true;
   };
--- 3modules/desktop/fonts.nix
+++ 3modules/desktop/fonts.nix
@@ -5,14 +5,14 @@ let
 in
 {
   fonts.fonts = with pkgs; lib.mkIf cfg.enable [
-    cozette
-    iosevka # TODO: replace, pulls in i686-incompatible dependencies
+    # cozette
+    # iosevka # pulls in i686-incompatible dependencies
     nerdfonts
     nwfonts
-    proggyfonts
+    # proggyfonts
     roboto
     roboto-slab
-    source-code-pro
+    # source-code-pro
     spleen
     win10fonts
   ];
