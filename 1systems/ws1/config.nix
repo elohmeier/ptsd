e819c77168b7c94e0edc38c1351e6196807719c9
Enno Richter <enno@nerdworks.de>
2021-11-30 21:48:09 +0100

    ws1: output cfg

--- 1systems/ws1/config.nix
+++ 1systems/ws1/config.nix
@@ -76,17 +76,30 @@ in
       audioAlert = "alsa_output.usb-LG_Electronics_Inc._USB_Audio-00.analog-stereo";
     };
     fontSize = 18.0;
+    waybar.primaryOutput = "Dell Inc. DELL P2415Q D8VXF96K09HB";
   };
 
   home-manager.users.mainUser = { pkgs, ... }: {
-    wayland.windowManager.sway = {
-      extraConfig = ''
-        output "Goldstar Company Ltd LG UltraFine 701NTAB7S144" pos 0 0 mode 4096x2304@59.999Hz scale 1
-        output "Dell Inc. DELL P2415Q D8VXF96K09HB" pos 0 2304 mode 3840x2160@59.997Hz scale 1
-        output "Dell Inc. DELL P2415Q D8VXF64G0LGL" pos 3840 2304 mode 3840x2160@59.997Hz scale 1
-      '';
+    wayland.windowManager.sway.config.output = {
+      "Goldstar Company Ltd LG UltraFine 701NTAB7S144" = {
+        pos = "0 0";
+        mode = "4096x2304@59.999Hz";
+        scale = "1";
+      };
+      "Dell Inc. DELL P2415Q D8VXF96K09HB" = {
+        #pos = "0 2304";
+        pos = "256 2304";
+        mode = "3840x2160@59.997Hz";
+        scale = "1";
+      };
+      "Dell Inc. DELL P2415Q D8VXF64G0LGL" = {
+        # pos = "3840 2304";
+        pos = "4096 360";
+        mode = "3840x2160@59.997Hz";
+        scale = "1";
+        transform = "270";
+      };
     };
-    ptsd.pcmanfm.enableRdpAssistant = true;
   };
 
   ptsd.nwbackup = {
