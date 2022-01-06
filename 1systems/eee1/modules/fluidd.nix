43807e7e1b87f625c6a31bcfafd3ccfe98c07016
Enno Richter <enno@nerdworks.de>
2022-01-06 18:45:58 +0100

    eee1: fluidd cfg

--- 1systems/eee1/modules/fluidd.nix
+++ 1systems/eee1/modules/fluidd.nix
@@ -1,10 +1,13 @@
-{ config, lib, pkgs, ... }: {
-
-
-
+{ config, lib, pkgs, ... }:
+{
   services.fluidd = {
     enable = true;
     hostName = "eee1.fritz.box";
+    nginx = {
+      # allow large uploads from slicer
+      extraConfig = ''
+        client_max_body_size 20M;
+      '';
+    };
   };
-
 }
