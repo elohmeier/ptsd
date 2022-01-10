6c537d2074267b1caa284f214523ffb78263bea7
Enno Richter <enno@nerdworks.de>
2022-01-10 15:06:46 +0100

    gowpcontactform: update mapping

--- 5pkgs/gowpcontactform/rcpts.go
+++ 5pkgs/gowpcontactform/rcpts.go
@@ -3,7 +3,7 @@ package main
 func GetRcpts() map[int]string {
 	return map[int]string{
 		11:   "earl.kuehne@fraam.de",
-		2328: "nicol.huckfeldt@fraam.de",
+		2328: "info@fraam.de",
 		2329: "susanne.halmburger@fraam.de",
 		2330: "klaus.stammerjohann@fraam.de",
 		2331: "enno.richter@fraam.de",
