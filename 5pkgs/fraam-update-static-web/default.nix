0228dcc35453e8ca6774c7a7964c9ab85951d238
Enno Richter <enno@nerdworks.de>
2022-01-17 16:54:10 +0100

    fraam-update-static-web: add pentest

--- 5pkgs/fraam-update-static-web/default.nix
+++ 5pkgs/fraam-update-static-web/default.nix
@@ -5,6 +5,7 @@ writers.writeDashBin "fraam-update-static-web" ''
   # fetch website
   ${wget}/bin/wget --mirror --page-requisites --no-parent --directory-prefix="$ROOT" --no-host-directories https://dev.fraam.de
   ${wget}/bin/wget --mirror --page-requisites --no-parent --directory-prefix="$ROOT" --no-host-directories https://dev.fraam.de/impressum/
+  ${wget}/bin/wget --mirror --page-requisites --no-parent --directory-prefix="$ROOT" --no-host-directories https://dev.fraam.de/pentests/
 
   # remove absolute links
   ${findutils}/bin/find "$ROOT" -type f -exec ${gnused}/bin/sed -i 's/https:\/\/dev.fraam.de\//\//g' {} +
