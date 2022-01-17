42dcea1196fddd437a51cbab8c4b6f5db5924504
Enno Richter <enno@nerdworks.de>
2022-01-17 16:55:30 +0100

    sxmo-utils: add deps

--- 5pkgs/sxmo-utils/default.nix
+++ 5pkgs/sxmo-utils/default.nix
@@ -1,5 +1,8 @@
-{ stdenv, fetchFromSourcehut, coreutils }:
+{ lib, stdenv, fetchFromSourcehut, coreutils, makeWrapper, jq, busybox, killall, bc }:
 
+let
+  binPath = lib.makeBinPath [ jq busybox ];
+in
 stdenv.mkDerivation rec {
   pname = "sxmo-utils";
   version = "1.7.1";
@@ -13,7 +16,17 @@ stdenv.mkDerivation rec {
 
   postPatch = ''
     substituteInPlace scripts/core/sxmo_common.sh \
-      --replace 'alias jq="gojq"' '#alias jq="gojq"'
+      --replace 'alias jq="gojq"' '#alias jq="gojq"' \
+      --replace busybox "${busybox}/bin/busybox"
+
+    substituteInPlace scripts/core/sxmo_wm.sh \
+      --replace 'jq' "${jq}/bin/jq"
+
+    substituteInPlace scripts/core/sxmo_screenlock.sh \
+      --replace 'killall' "${killall}/bin/killall"
+
+    substituteInPlace scripts/core/sxmo_setled.sh \
+      --replace 'bc' "${bc}/bin/bc"
 
     substituteInPlace configs/udev/90-sxmo.rules \
       --replace '/usr/bin/sxmo_statusbarupdate.sh' "$out/bin/sxmo_statusbarupdate.sh" \
@@ -22,6 +35,8 @@ stdenv.mkDerivation rec {
   '';
 
   installFlags = [ "DESTDIR=$(out)" ];
+
+  nativeBuildInputs = [ makeWrapper ];
 
   postInstall = ''
     mv $out/usr/* $out/
