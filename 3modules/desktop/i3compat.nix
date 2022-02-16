ed1615aadefd73156d534ebad4a7ce3006997faa
Enno Richter <enno@nerdworks.de>
2022-02-16 22:10:30 +0100

    i3compat: update i3status

--- 3modules/desktop/i3compat.nix
+++ 3modules/desktop/i3compat.nix
@@ -93,17 +93,28 @@ in
 
         programs.i3status = {
           enable = true;
-          enableDefault = true;
+          enableDefault = false;
+          general = {
+            colors = true;
+            interval = 5;
+          };
           modules = {
+            ipv6.position = 1;
 
-            "wireless _first_".settings = {
-              format_up = "  (%quality at %essid) %ip";
-              format_down = "  down";
+            "wireless _first_" = {
+              position = 2;
+              settings = {
+                format_up = "  (%quality at %essid) %ip";
+                format_down = "  down";
+              };
             };
 
-            "ethernet _first_".settings = {
-              format_up = "ﯱ e: %ip (%speed)";
-              format_down = " e: down";
+            "ethernet _first_" = {
+              position = 3;
+              settings = {
+                format_up = "ﯱ e: %ip (%speed)";
+                format_down = " e: down";
+              };
             };
 
             "ethernet nwvpn" = {
@@ -122,7 +133,15 @@ in
               };
             };
 
-            "disk /".settings.format = " / %avail";
+            "battery all" = {
+              position = 4;
+              settings = { format = "%status %percentage %remaining"; };
+            };
+
+            "disk /" = {
+              position = 5;
+              settings.format = " / %avail";
+            };
 
             "disk /home" = {
               position = 5;
@@ -139,8 +158,27 @@ in
               settings.format = " xrd %avail";
             };
 
-            load.settings.format = " %1min";
-            memory.settings.format = " %used | %available";
+            load = {
+              position = 6;
+              settings = {
+                format = " %1min";
+              };
+            };
+
+            memory = {
+              position = 7;
+              settings = {
+                format = " %used | %available";
+                threshold_degraded = "1G";
+                format_degraded = "MEMORY < %available";
+              };
+            };
+
+            "tztime local" = {
+              position = 8;
+              settings = { format = "%Y-%m-%d %H:%M:%S"; };
+            };
+
           };
         };
 
