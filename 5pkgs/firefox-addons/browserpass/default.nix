83156fd7daa9c111e94af16fce3a19ad24e900d6
Enno Richter <enno@nerdworks.de>
2022-03-23 17:30:20 +0100

    firefox-addons: fix messaginghost integration

--- 5pkgs/firefox-addons/browserpass/default.nix
+++ 5pkgs/firefox-addons/browserpass/default.nix
@@ -1,7 +1,8 @@
 { lib, fetchFirefoxAddon }:
 
 fetchFirefoxAddon {
-  name = "browserpass-ce";
+  name = "browserpass";
+  fixedExtid = "browserpass@maximbaz.com"; # required for nativeMessagingHost integration
   url = "https://addons.mozilla.org/firefox/downloads/file/3711209/browserpass-3.7.2-fx.xpi";
   sha256 = "sha256-sXgUBbRvMnRpeIW1MTkmTcoqtW/8RDXAkxAq1evFkpc=";
 }
