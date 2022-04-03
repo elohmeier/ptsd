51a3f8d80abfc5ad45d26db0c13e6096b212d222
Enno Richter <enno@nerdworks.de>
2022-04-03 08:48:40 +0200

    chromium: use ungoogled

--- 2configs/home/chromium.nix
+++ 2configs/home/chromium.nix
@@ -1,6 +1,7 @@
 { config, lib, pkgs, nixosConfig, ... }: {
   programs.chromium = {
     enable = !nixosConfig.ptsd.minimal;
+    package = pkgs.ungoogled-chromium;
     extensions = [
       { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock origin
       { id = "naepdomgkenhinolocfifgehidddafch"; } # browserpass
