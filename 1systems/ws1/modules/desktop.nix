0e37b5933333a08a67800a3d5452a762a91f568f
Enno Richter <enno@nerdworks.de>
2022-04-26 00:40:04 +0200

    ws1: logseq sync

--- 1systems/ws1/modules/desktop.nix
+++ 1systems/ws1/modules/desktop.nix
@@ -1,4 +1,15 @@
-{ config, lib, pkgs, ... }: {
+{ config, lib, pkgs, ... }:
+
+let
+  logseq-sync-git = pkgs.writeShellScriptBin "logseq-sync-git" ''
+    set -e
+    REPO=/home/enno/repos/logseq
+    export GIT_SSH_COMMAND="ssh -i /home/enno/.ssh/id_ed25519"
+    ${pkgs.git}/bin/git -C "$REPO" pull --rebase=merges
+    ${pkgs.git}/bin/git -C "$REPO" push origin main
+  '';
+in
+{
 
   ptsd.desktop = {
     enable = true;
@@ -15,26 +26,53 @@
   home-manager.users.mainUser = { pkgs, ... }: {
     wayland.windowManager.sway.config.output = {
       "Goldstar Company Ltd LG UltraFine 701NTAB7S144" = {
-        pos = "0 0";
+        pos = "4320 0";
+        transform = "270";
         mode = "4096x2304@59.999Hz";
         scale = "1";
-        bg = "/home/enno/Pocket/JI2A5332.JPG fill";
+        bg = "/mnt/photos/photos/originals/eosr6/2022/2022-04/2022-04-12/JI2A8211.JPG fill";
+        #bg = "/mnt/photos/photos/originals/eosr6/2022/2022-04/2022-04-06/JI2A7930.JPG fill"; # not rotated
       };
       "Dell Inc. DELL P2415Q D8VXF96K09HB" = {
-        #pos = "0 2304";
-        pos = "256 2304";
+        pos = "0 130";
+        transform = "90";
         mode = "3840x2160@59.997Hz";
         scale = "1";
-        bg = "/home/enno/Downloads/JI2A4943.JPG fill";
+        bg = "/mnt/photos/photos/originals/eosr6/2022/2022-04/2022-04-12/JI2A8211.JPG fill";
       };
       "Dell Inc. DELL P2415Q D8VXF64G0LGL" = {
-        # pos = "3840 2304";
-        pos = "4096 360";
+        pos = "2160 130";
+        transform = "90";
         mode = "3840x2160@59.997Hz";
         scale = "1";
-        transform = "270";
-        bg = "/home/enno/Downloads/JI2A5337_90.JPG fill";
+        bg = "/mnt/photos/photos/originals/eosr6/2022/2022-04/2022-04-12/JI2A8211.JPG fill";
+        #bg = "/mnt/photos/photos/originals/eosr6/2022/2022-04/2022-04-01/JI2A7676.JPG fill"; # not rotated
       };
+    };
+
+    home.packages = [ logseq-sync-git ];
+
+    programs.ssh.matchBlocks."git.nerdworks.de" = {
+      hostname = "git.nerdworks.de";
+      identityFile = "/home/enno/.ssh/id_ed25519";
+    };
+
+    systemd.user.services.logseq-sync-git = {
+      Unit.Description = "Sync Logseq git repo";
+      Service = {
+        CPUSchedulingPolicy = "idle";
+        IOSchedulingClass = "idle";
+        ExecStart = "${logseq-sync-git}/bin/logseq-sync-git";
+      };
+    };
+
+    systemd.user.timers.logseq-sync-git = {
+      Unit.Description = "Sync Logseq git repo periodically";
+      Timer = {
+        OnBootSec = "1min";
+        OnUnitInactiveSec = "1min";
+      };
+      Install.WantedBy = [ "timers.target" ];
     };
   };
 
