a77cb21d771a1b9eb1adc8a864e9885a62dd2cae
Enno Richter <enno@nerdworks.de>
2022-05-22 00:18:17 +0200

    workstation: add file-renamer for zathura

--- 2configs/profiles/workstation/default.nix
+++ 2configs/profiles/workstation/default.nix
@@ -120,7 +120,36 @@ in
         enable = !nixosConfig.ptsd.minimal;
         extraConfig =
           let
-            cmd = desktopCfg.term.execFloating "${pkgs.file-renamer} \"%\"" "";
+            file-renamer = pkgs.writers.writePython3 "file-renamer" { } ''
+              import argparse
+              import readline
+              from pathlib import Path
+
+
+              def rlinput(prompt, prefill=""):
+                  readline.set_startup_hook(lambda: readline.insert_text(prefill))
+                  try:
+                      return input(prompt)
+                  finally:
+                      readline.set_startup_hook()
+
+
+              def main():
+                  parser = argparse.ArgumentParser()
+                  parser.add_argument("filename")
+                  args = parser.parse_args()
+                  f = Path(args.filename)
+                  if not f.exists():
+                      raise FileNotFoundError(f)
+
+                  new_f = f.parent / rlinput("filename: ", f.name)
+                  f.rename(new_f)
+
+
+              if __name__ == "__main__":
+                  main()
+            '';
+            cmd = desktopCfg.term.execFloating "${file-renamer} \"%\"" "";
           in
           ''
             map <C-o> exec '${cmd}'
