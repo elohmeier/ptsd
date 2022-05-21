84cf940b6427c34fc2190f04d1e0ee587f15ac0f
Enno Richter <enno@nerdworks.de>
2022-05-22 00:16:15 +0200

    chromium: use google-chrome-bin

--- 2configs/home/chromium.nix
+++ 2configs/home/chromium.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, nixosConfig, ... }: {
   programs.chromium = {
     enable = !nixosConfig.ptsd.minimal;
-    package = pkgs.ungoogled-chromium;
+    package = pkgs.google-chrome;
     extensions = [
       { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock origin
       { id = "naepdomgkenhinolocfifgehidddafch"; } # browserpass
