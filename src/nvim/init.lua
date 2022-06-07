8923be70a9c7f93eba0f0f968f1e84fdd6985e71
Enno Richter <enno@nerdworks.de>
2022-06-07 12:28:21 +0200

    nvim: buffer navigation keybindings

--- src/nvim/init.lua
+++ src/nvim/init.lua
@@ -17,4 +17,13 @@ require("plugincfg.nnn");
 require("plugincfg.telescope");
 require("plugincfg.treesitter");
 
+-- open empty buffer
+vim.api.nvim_set_keymap("n", "<leader>m", "<cmd>enew<cr>", {noremap = true, silent = true})
+
+-- buffer navigation
+vim.api.nvim_set_keymap("n", "<leader>l", "<cmd>bnext<cr>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>h", "<cmd>bprevious<cr>", {noremap = true, silent = true})
+
+-- close current buffer and switch to previous buffer
+vim.api.nvim_set_keymap("n", "<leader>bq", "<cmd>bp<bar>bd #<cr>", {noremap = true, silent = true})
 
