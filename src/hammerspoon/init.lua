6873a862368d20b31ffd318046d32e731ee3dd99
Enno Richter <enno@nerdworks.de>
2022-06-08 06:54:55 +0200

    hammerspoon: window split

--- src/hammerspoon/init.lua
+++ src/hammerspoon/init.lua
@@ -1,3 +1,12 @@
 hs.hotkey.bind({"option"}, "return", function()
   hs.application.launchOrFocus("/Users/enno/.nix-profile/bin/alacritty")
 end)
+
+hs.loadSpoon("WindowHalfsAndThirds")
+
+-- https://www.hammerspoon.org/Spoons/WindowHalfsAndThirds.html#bindHotkeys
+spoon.WindowHalfsAndThirds:bindHotkeys({
+  left_half = {{"alt", "shift"}, "Left"},
+  right_half = {{"alt", "shift"}, "Right"},
+})
+
