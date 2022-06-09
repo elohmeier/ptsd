7b05565cc8cfad05d3ad9f0fc870fddee500c1a4
Enno Richter <enno@nerdworks.de>
2022-06-09 07:55:27 +0200

    update hammerspoon cfg

--- src/hammerspoon/init.lua
+++ src/hammerspoon/init.lua
@@ -6,7 +6,23 @@ hs.loadSpoon("WindowHalfsAndThirds")
 
 -- https://www.hammerspoon.org/Spoons/WindowHalfsAndThirds.html#bindHotkeys
 spoon.WindowHalfsAndThirds:bindHotkeys({
-  left_half = {{"alt", "shift"}, "Left"},
-  right_half = {{"alt", "shift"}, "Right"},
+    left_half   = { {"ctrl",        "cmd"}, "Left" },
+    right_half  = { {"ctrl",        "cmd"}, "Right" },
+    top_half    = { {"ctrl",        "cmd"}, "Up" },
+    bottom_half = { {"ctrl",        "cmd"}, "Down" },
+    third_left  = { {"ctrl", "alt"       }, "Left" },
+    third_right = { {"ctrl", "alt"       }, "Right" },
+    third_up    = { {"ctrl", "alt"       }, "Up" },
+    third_down  = { {"ctrl", "alt"       }, "Down" },
+    top_left    = { {"ctrl",        "cmd"}, "1" },
+    top_right   = { {"ctrl",        "cmd"}, "2" },
+    bottom_left = { {"ctrl",        "cmd"}, "3" },
+    bottom_right= { {"ctrl",        "cmd"}, "4" },
+    max_toggle  = { {"ctrl", "alt", "cmd"}, "f" },
+    max         = { {"ctrl", "alt", "cmd"}, "Up" },
+    undo        = { {        "alt", "cmd"}, "y" },
+    center      = { {        "alt", "cmd"}, "c" },
+    larger      = { {        "alt", "cmd", "shift"}, "Right" },
+    smaller     = { {        "alt", "cmd", "shift"}, "Left" },
 })
 
