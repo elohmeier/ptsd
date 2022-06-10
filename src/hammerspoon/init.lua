7177e64c33886ab47fb93658625e38217f6b70b3
Enno Richter <enno@nerdworks.de>
2022-06-10 06:40:53 +0200

    hammerspoon: no focus of alacritty instances

--- src/hammerspoon/init.lua
+++ src/hammerspoon/init.lua
@@ -1,28 +1,29 @@
 hs.hotkey.bind({"option"}, "return", function()
-  hs.application.launchOrFocus("/Users/enno/.nix-profile/bin/alacritty")
+    -- hs.application.launchOrFocus("/Users/enno/.nix-profile/bin/alacritty") -- focuses old instances
+    -- hs.execute("/Users/enno/.nix-profile/bin/alacritty") -- blocking
+    io.popen("/Users/enno/.nix-profile/bin/alacritty")
 end)
 
 hs.loadSpoon("WindowHalfsAndThirds")
 
 -- https://www.hammerspoon.org/Spoons/WindowHalfsAndThirds.html#bindHotkeys
 spoon.WindowHalfsAndThirds:bindHotkeys({
-    left_half   = { {"ctrl",        "cmd"}, "Left" },
-    right_half  = { {"ctrl",        "cmd"}, "Right" },
-    top_half    = { {"ctrl",        "cmd"}, "Up" },
-    bottom_half = { {"ctrl",        "cmd"}, "Down" },
-    third_left  = { {"ctrl", "alt"       }, "Left" },
-    third_right = { {"ctrl", "alt"       }, "Right" },
-    third_up    = { {"ctrl", "alt"       }, "Up" },
-    third_down  = { {"ctrl", "alt"       }, "Down" },
-    top_left    = { {"ctrl",        "cmd"}, "1" },
-    top_right   = { {"ctrl",        "cmd"}, "2" },
-    bottom_left = { {"ctrl",        "cmd"}, "3" },
-    bottom_right= { {"ctrl",        "cmd"}, "4" },
-    max_toggle  = { {"ctrl", "alt", "cmd"}, "f" },
-    max         = { {"ctrl", "alt", "cmd"}, "Up" },
-    undo        = { {        "alt", "cmd"}, "y" },
-    center      = { {        "alt", "cmd"}, "c" },
-    larger      = { {        "alt", "cmd", "shift"}, "Right" },
-    smaller     = { {        "alt", "cmd", "shift"}, "Left" },
+    left_half = {{"ctrl", "cmd"}, "Left"},
+    right_half = {{"ctrl", "cmd"}, "Right"},
+    top_half = {{"ctrl", "cmd"}, "Up"},
+    bottom_half = {{"ctrl", "cmd"}, "Down"},
+    third_left = {{"ctrl", "alt"}, "Left"},
+    third_right = {{"ctrl", "alt"}, "Right"},
+    third_up = {{"ctrl", "alt"}, "Up"},
+    third_down = {{"ctrl", "alt"}, "Down"},
+    top_left = {{"ctrl", "cmd"}, "1"},
+    top_right = {{"ctrl", "cmd"}, "2"},
+    bottom_left = {{"ctrl", "cmd"}, "3"},
+    bottom_right = {{"ctrl", "cmd"}, "4"},
+    max_toggle = {{"ctrl", "alt", "cmd"}, "f"},
+    max = {{"ctrl", "alt", "cmd"}, "Up"},
+    undo = {{"alt", "cmd"}, "y"},
+    center = {{"alt", "cmd"}, "c"},
+    larger = {{"alt", "cmd", "shift"}, "Right"},
+    smaller = {{"alt", "cmd", "shift"}, "Left"}
 })
-
