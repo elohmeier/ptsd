4e511c945c77784f8c4468d54354cabcd5359bf9
Enno Richter <enno@nerdworks.de>
2022-06-15 07:18:57 +0200

    configure selenized-black scheme

--- 2configs/home/foot.nix
+++ 2configs/home/foot.nix
@@ -9,28 +9,31 @@
         dpi-aware = lib.mkDefault "no";
       };
       scrollback.lines = 50000;
+      cursor.color = with config.ptsd.style.colors; "${background} ${base0F}";
 
       colors = with config.ptsd.style.colors; {
-        background = base00;
-        foreground = base05;
+        background = if background != "" then background else base00;
+        foreground = if foreground != "" then foreground else base05;
 
-        regular0 = base00;
-        regular1 = base08;
-        regular2 = base0B;
-        regular3 = base0A;
-        regular4 = base0D;
-        regular5 = base0E;
-        regular6 = base0C;
-        regular7 = base05;
+        # Normal colors
+        regular0 = base00; # Black, could also be base01
+        regular1 = base08; # Red
+        regular2 = base0B; # Green
+        regular3 = base0A; # Yellow
+        regular4 = base0D; # Blue
+        regular5 = base0E; # Magenta
+        regular6 = base0C; # Cyan
+        regular7 = base05; # White
 
+        # Bright colors
         bright0 = base03;
         bright1 = base09;
-        bright2 = base0F;
-        bright3 = base0E;
-        bright4 = base0C;
-        bright5 = base01;
-        bright6 = base0D;
-        bright7 = base02;
+        bright2 = base01;
+        bright3 = base02;
+        bright4 = base04;
+        bright5 = base06;
+        bright6 = base0F;
+        bright7 = base07;
       };
     };
   };
--- 3modules/home/style.nix
+++ 3modules/home/style.nix
@@ -25,10 +25,35 @@ in
     # see https://github.com/base16-project/base16-schemes
     themeFile = mkOption {
       type = types.path;
-      default = ./base16-schemes/windows-10.yaml;
+      default = ./base16-schemes/selenized-black.yaml;
     };
 
-    colors = mkOption { type = types.attrs; };
+    colors = {
+      # optional
+      background = mkOption { type = types.str; default = ""; };
+      foreground = mkOption { type = types.str; default = ""; };
+
+      base00 = mkOption { type = types.str; };
+      base01 = mkOption { type = types.str; };
+      base02 = mkOption { type = types.str; };
+      base03 = mkOption { type = types.str; };
+      base04 = mkOption { type = types.str; };
+      base05 = mkOption { type = types.str; };
+      base06 = mkOption { type = types.str; };
+      base07 = mkOption { type = types.str; };
+      base08 = mkOption { type = types.str; };
+      base09 = mkOption { type = types.str; };
+      base0A = mkOption { type = types.str; };
+      base0B = mkOption { type = types.str; };
+      base0C = mkOption { type = types.str; };
+      base0D = mkOption { type = types.str; };
+      base0E = mkOption { type = types.str; };
+      base0F = mkOption { type = types.str; };
+
+      # metadata
+      author = mkOption { type = types.str; };
+      scheme = mkOption { type = types.str; };
+    };
 
   };
 
