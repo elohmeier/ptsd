197aee0b2efb227d10ecbacc9c5db5b62f78b611
Enno Richter <enno@nerdworks.de>
2022-07-18 21:25:58 +0200

    maxos-fix-filefoldernames: add eliminate & fix recursion

--- 4scripts/macos-fix-filefoldernames.py
+++ 4scripts/macos-fix-filefoldernames.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 import argparse
-
+import os
 from pathlib import Path
 
-repl_uml = {
+fix_uml = {
     # remove "Combining diaeresis"
     "a\u0308": "ä",
     "o\u0308": "ö",
@@ -16,26 +16,72 @@ repl_uml = {
     "e\u0301": "é",
 }
 
+eleminate_uml = {
+    # remove "Combining diaeresis"
+    "a\u0308": "ae",
+    "o\u0308": "oe",
+    "u\u0308": "ue",
+    "A\u0308": "Ae",
+    "O\u0308": "Oe",
+    "U\u0308": "Ue",
+    # remove "acute accent"
+    "e\u0301": "e",
+    "ä": "ae",
+    "ö": "oe",
+    "ü": "ue",
+    "Ä": "Ae",
+    "Ö": "Oe",
+    "Ü": "Ue",
+    "é": "e",
+    "ß": "ss",
+}
 
-def fix_uml(filename):
+
+def replace(filename: str, replace_dict: dict) -> str:
+    """Replace symbols in filename"""
     new = filename
-    for k, v in repl_uml.items():
+    for k, v in replace_dict.items():
         new = new.replace(k, v)
     return new
 
 
-def format_file_folder_names(path: Path):
+def rename_folders(path: Path, replace_dict: dict, dry_run: bool) -> None:
+    """Rename folders"""
+    for subdir, _, _ in os.walk(path, topdown=False):
+        old_bn = os.path.basename(subdir)
+        new_bn = replace(old_bn, replace_dict)
+        if old_bn != new_bn:
+            new = os.path.join(os.path.dirname(subdir), new_bn)
+            print(subdir, ">>>", new)
+            if not dry_run:
+                os.rename(subdir, new)
+
+
+def rename_files(path: Path, replace_dict: dict, dry_run: bool) -> None:
     for p in path.glob("**/*"):
-        old = p.name
-        new = fix_uml(old)
-        if old != new:
-            print(old, ">>>", new)
-            p.rename(p.parent / new)
+        if p.is_file():
+            old = p.name
+            new = replace(old, replace_dict)
+            if old != new:
+                print(old, ">>>", new)
+                if not dry_run:
+                    p.rename(p.parent / new)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("--eliminate", "-e", action="store_true")
+    parser.add_argument("--files-only", "-f", action="store_true")
+    parser.add_argument("--folders-only", "-d", action="store_true")
+    parser.add_argument("--dry-run", "-n", action="store_true")
     parser.add_argument("dir")
     args = parser.parse_args()
 
-    format_file_folder_names(Path(args.dir))
+    repl_dict = eleminate_uml if args.eliminate else fix_uml
+    dir = Path(args.dir)
+
+    if not args.files_only:
+        rename_folders(dir, repl_dict, args.dry_run)
+
+    if not args.folders_only:
+        rename_files(dir, repl_dict, args.dry_run)
