6e78000717ca33641e62f811ff55265b4b04553f
Enno Richter <enno@nerdworks.de>
2022-07-26 21:11:48 +0200

    prometheus/node: use w/o nwtraefik

--- 2configs/prometheus/node.nix
+++ 2configs/prometheus/node.nix
@@ -1,42 +1,15 @@
 { config, lib, pkgs, ... }:
 let
-  port = 9100;
+  universe = import ../universe.nix;
 in
 {
   services.prometheus.exporters.node = {
     enable = true;
-    listenAddress = "127.0.0.1";
+    listenAddress = universe.hosts."${config.networking.hostName}".nets.tailscale.ip4.addr;
     port = config.ptsd.ports.prometheus-node;
     enabledCollectors = import ./node_collectors.nix;
     extraFlags = [
       "--collector.textfile.directory=/var/log"
     ];
   };
-
-  ptsd.nwtraefik = {
-    enable = true;
-    entryPoints = {
-      nwvpn-prometheus-http = {
-        address = "${config.ptsd.wireguard.networks.nwvpn.ip}:${toString port}";
-      };
-    };
-    middlewares = {
-      prom-stripprefix = {
-        stripprefixregex = {
-          regex = "/[a-zA-Z0-9]+/[a-zA-Z0-9]+/";
-        };
-      };
-    };
-    services = [
-      {
-        name = "prometheus-node";
-        entryPoints = [ "nwvpn-prometheus-http" ];
-        rule = "PathPrefix(`/${config.networking.hostName}/node`) && Host(`${config.ptsd.wireguard.networks.nwvpn.ip}`)";
-        tls = false;
-        extraMiddlewares = [ "prom-stripprefix" ];
-      }
-    ];
-  };
-
-  networking.firewall.interfaces.nwvpn.allowedTCPPorts = [ port ];
 }
