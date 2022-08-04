e3b435ce609d35c5491e7f43c6a7e39cf19aaa22
Enno Richter <enno@nerdworks.de>
2022-08-04 16:45:52 +0200

    nas1: syncthing cfg, reorder

--- 1systems/nas1/modules/syncthing.nix
+++ 1systems/nas1/modules/syncthing.nix
@@ -6,115 +6,38 @@ let
   universe = import ../../../2configs/universe.nix;
 in
 {
-  ptsd.secrets.files = {
-    "syncthing.key" = { };
-    "syncthing.crt" = { };
-  };
+  systemd.services."syncthing.service".serviceConfig.LoadCredential = [
+    "syncthing.key:/var/src/secrets/syncthing.key"
+    "syncthing.crt:/var/src/secrets/syncthing.crt"
+  ];
 
   services.syncthing = {
     enable = true;
 
-    key = config.ptsd.secrets.files."syncthing.key".path;
-    cert = config.ptsd.secrets.files."syncthing.crt".path;
+    key = "/run/credentials/syncthing.service/syncthing.key";
+    cert = "/run/credentials/syncthing.service/syncthing.crt";
     devices = mapAttrs (_: hostcfg: hostcfg.syncthing) (filterAttrs (_: hostcfg: hasAttr "syncthing" hostcfg) universe.hosts);
 
     folders = {
-      "/tank/enc/rawphotos/2021-06 mopedtour" = {
-        label = "2021-06 mopedtour";
-        id = "ieyohHo7uang";
-        devices = [ "ext-arvid" "ext-arvid-laptop" ];
-      };
-      "/tank/enc/rawphotos/photos" = {
-        label = "photos";
-        id = "rqvar-xdhbm";
-        devices = [ "ws1" "mb4" ];
-      };
-      "/tank/enc/enno/FPV" = {
-        label = "enno/FPV";
-        id = "xxdwi-yom6n";
-        devices = [ "mb4" "ws1" ];
-      };
-      "/tank/enc/enno/Pocket" = {
-        label = "enno/Pocket";
-        id = "hmekh-kgprn";
-        devices = [ "mb4" "ws1" ];
-      };
-      "/tank/enc/enno/LuNo" = {
-        label = "enno/LuNo";
-        id = "3ull9-9deg4";
-        devices = [ "mb1" "mb3" "mb4" "ws1" ];
-      };
-      "/tank/enc/enno/Scans" = {
-        label = "enno/Scans";
-        id = "ezjwj-xgnhe";
-        devices = [ "mb4" "ws1" "iph3" ];
-      };
-      "/tank/enc/enno/Templates" = {
-        label = "enno/Templates";
-        id = "gnwqu-yt7qc";
-        devices = [ "mb4" "ws1" ];
-      };
-      "/tank/enc/repos" = {
-        label = "enno/repos";
-        id = "yqa69-2zjmt";
-        devices = [ "pine2" "mb4" "ws1" ];
-        ignorePerms = false;
-      };
-      "/tank/enc/enno/iOS" = {
-        label = "enno/iOS";
-        id = "qm9ln-btyqu";
-        devices = [ "iph3" "mb4" "ws1" ];
-      };
-      "/tank/enc/enno/Lightroom-Export" = {
-        label = "enno/Lightroom-Export";
-        id = "uxsxc-bjqrg";
-        devices = [ "iph3" ];
-        ignoreDelete = true;
-      };
-
-      "/tank/enc/luisa/Bilder" = {
-        label = "luisa/Bilder";
-        id = "ugmai-ti6vl";
-        devices = [ "mb1" "mb3" "ws1" "mb4" ];
-      };
-      "/tank/enc/luisa/Dokumente" = {
-        label = "luisa/Dokumente";
-        id = "sqkfd-m9he7";
-        devices = [ "mb4" "mb1" "mb3" "ws1" ];
-      };
-      "/tank/enc/luisa/Musik" = {
-        label = "luisa/Musik";
-        id = "zvffu-ff92z";
-        devices = [ "mb1" "mb3" "ws1" "mb4" ];
-      };
-      "/tank/enc/luisa/Scans" = {
-        label = "luisa/Scans";
-        id = "dnryo-kz7io";
-        devices = [ "mb4" "mb1" "mb3" "ws1" ];
-      };
-
-      "/var/cache/photoprism" = {
-        label = "photoprism-cache";
-        id = "tsfyr-53d26";
-        devices = [ "ws1" "mb4" ];
-      };
-      "/var/lib/photoprism" = {
-        label = "photoprism-lib";
-        id = "3tf3k-nohyy";
-        devices = [ "ws1" "mb4" ];
-      };
-
-      "/tank/enc/media" = {
-        label = "media";
-        id = "zfruo-ytfi2";
-        devices = [ "mb4" ];
-      };
-
-      "/tank/enc/roms" = {
-        label = "roms";
-        id = "avcjn-tyzyp";
-        devices = [ "mb4" ];
-      };
+      "/tank/enc/enno/Documents" = { label = "enno/Documents"; id = "hmekh-kgprn"; devices = [ "mb4" ]; };
+      "/tank/enc/enno/FPV" = { label = "enno/FPV"; id = "xxdwi-yom6n"; devices = [ "mb4" ]; };
+      "/tank/enc/enno/Lightroom-Export" = { label = "enno/Lightroom-Export"; id = "uxsxc-bjqrg"; devices = [ "iph3" ]; ignoreDelete = true; };
+      "/tank/enc/enno/LuNo" = { label = "enno/LuNo"; id = "3ull9-9deg4"; devices = [ "mb1" "mb3" "mb4" ]; };
+      "/tank/enc/enno/Recordings" = { label = "enno/Recordings"; id = "qihz9-vbq6o"; devices = [ "mb4" ]; };
+      "/tank/enc/enno/Scans" = { label = "enno/Scans"; id = "ezjwj-xgnhe"; devices = [ "mb4" "iph3" ]; };
+      "/tank/enc/enno/Templates" = { label = "enno/Templates"; id = "gnwqu-yt7qc"; devices = [ "mb4" ]; };
+      "/tank/enc/enno/iOS" = { label = "enno/iOS"; id = "qm9ln-btyqu"; devices = [ "iph3" "mb4" ]; };
+      "/tank/enc/luisa/Bilder" = { label = "luisa/Bilder"; id = "ugmai-ti6vl"; devices = [ "mb1" "mb3" "mb4" ]; };
+      "/tank/enc/luisa/Dokumente" = { label = "luisa/Dokumente"; id = "sqkfd-m9he7"; devices = [ "mb4" "mb1" "mb3" ]; };
+      "/tank/enc/luisa/Musik" = { label = "luisa/Musik"; id = "zvffu-ff92z"; devices = [ "mb1" "mb3" "mb4" ]; };
+      "/tank/enc/luisa/Scans" = { label = "luisa/Scans"; id = "dnryo-kz7io"; devices = [ "mb4" "mb1" "mb3" ]; };
+      "/tank/enc/media" = { label = "media"; id = "zfruo-ytfi2"; devices = [ "mb4" ]; };
+      "/tank/enc/rawphotos/2021-06 mopedtour" = { label = "2021-06 mopedtour"; id = "ieyohHo7uang"; devices = [ "ext-arvid" "ext-arvid-laptop" ]; };
+      "/tank/enc/rawphotos/photos" = { label = "photos"; id = "rqvar-xdhbm"; devices = [ "mb4" ]; };
+      "/tank/enc/repos" = { label = "enno/repos"; id = "yqa69-2zjmt"; devices = [ "pine2" "mb4" ]; ignorePerms = false; };
+      "/tank/enc/roms" = { label = "roms"; id = "avcjn-tyzyp"; devices = [ "mb4" ]; };
+      "/var/cache/photoprism" = { label = "photoprism-cache"; id = "tsfyr-53d26"; devices = [ "mb4" ]; };
+      "/var/lib/photoprism" = { label = "photoprism-lib"; id = "3tf3k-nohyy"; devices = [ "mb4" ]; };
     };
   };
 
