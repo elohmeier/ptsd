166675035efcf0ec759d5cc5a722e3930326a8b1
Enno Richter <enno@nerdworks.de>
2022-09-07 11:44:33 +0200

    snippets: add python project templates

--- src/snippets/nix.lua
+++ src/snippets/nix.lua
@@ -79,5 +79,100 @@ buildPythonPackage rec {
   doCheck = false;
 
   propagatedBuildInputs = [ ];
+}]]), parse("nix-flake-django-venv", [[
+{
+  description = "$1";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "i686-linux" "aarch64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+    {
+      devShell = forAllSystems
+        (system: (with (import nixpkgs { inherit system; });
+        let
+          py3 = python3.override
+            {
+              packageOverrides = self: super: rec {
+                django = self.django_3;
+                django-stubs-ext = self.buildPythonPackage rec {
+                  pname = "django-stubs-ext";
+                  version = "0.5.0";
+                  src = fetchFromGitHub {
+                    owner = "typeddjango";
+                    repo = "django-stubs";
+                    rev = "django-stubs-ext@${version}";
+                    sha256 = "sha256-Y3FZIjQ9li51LCf//zqvxfYQ7qJnvV0rs/qWrQSThvU=";
+                  };
+                  sourceRoot = "${src.name}/django_stubs_ext";
+                  propagatedBuildInputs = with super; [ django typing-extensions ];
+                  doCheck = false;
+                };
+                django-stubs = self.buildPythonPackage rec {
+                  pname = "django-stubs";
+                  version = "1.12.0";
+                  src = self.fetchPypi {
+                    inherit pname version;
+                    sha256 = "sha256-6os10NpJ97LumaeRJfGUPgM0Md0RRybWZDzDXeYZIw4=";
+                  };
+                  propagatedBuildInputs = with super; [ django mypy django-stubs-ext tomli typing-extensions types-pytz types-pyyaml ];
+                  doCheck = false;
+                };
+                pyinstrument = self.buildPythonPackage rec {
+                  pname = "pyinstrument";
+                  version = "4.1.1";
+                  src = self.fetchPypi {
+                    inherit pname version;
+                    sha256 = "sha256-HcJ5HSzS/TRZy1UBAASlzCqai0YloMvqRaSxrrviw6I=";
+                  };
+                  doCheck = false;
+                };
+              };
+            };
+          venv = py3.withPackages (ps: with ps; [
+            django
+            django-debug-toolbar
+            django-stubs
+            django_environ
+            pyinstrument
+            whitenoise
+          ]);
+        in
+        mkShell {
+          buildInputs = with pkgs; [
+            venv
+          ];
+          DEBUG = "1";
+          PYTHONPATH = "${venv}/${py3.sitePackages}";
+        }));
+    };
+}]]), parse("nix-flake-python-venv", [[
+{
+  description = "$1";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "i686-linux" "aarch64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+    {
+      devShell = forAllSystems
+        (system: (with (import nixpkgs { inherit system; });
+        let
+          py3 = python3.override
+            {
+              packageOverrides = self: super: rec { };
+            };
+          venv = py3.withPackages (ps: with ps; [ ]);
+        in
+        mkShell {
+          buildInputs = with pkgs; [ venv ];
+        }));
+    };
 }]])
 }
