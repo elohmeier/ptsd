374d19bc502afa6e15d1e67af86107c49ada62cc
Enno Richter <enno@nerdworks.de>
2022-10-26 21:56:31 +0200

    utmvm: add rosetta mount

--- 2configs/utmvm.nix
+++ 2configs/utmvm.nix
@@ -6,10 +6,14 @@
     ./devenv.nix
   ];
 
-  boot = {
-    binfmt.emulatedSystems = [ "x86_64-linux" ];
-    initrd.availableKernelModules = [ "xhci_pci" "virtio_pci" "usbhid" "usb_storage" "sr_mod" ];
-  };
+  boot.initrd.availableKernelModules = [
+    "sr_mod"
+    "usb_storage"
+    "usbhid"
+    "virtio_pci"
+    "virtiofs"
+    "xhci_pci"
+  ];
 
   networking = {
     firewall.trustedInterfaces = [ "enp0s6" ];
@@ -63,4 +67,24 @@
   services.openssh.extraConfig = ''
     AcceptEnv TERM_PROGRAM LC_TERMINAL
   '';
+
+  fileSystems."/run/rosetta" = {
+    device = "rosetta";
+    fsType = "virtiofs";
+  };
+
+  nix.settings = {
+    extra-platforms = [ "x86_64-linux" ];
+    extra-sandbox-paths = [ "/run/rosetta" "/run/binfmt" ];
+  };
+
+  boot.binfmt.registrations."rosetta" = {
+    # based on https://developer.apple.com/documentation/virtualization/running_intel_binaries_in_linux_vms_with_rosetta#3978495
+    interpreter = "/run/rosetta/rosetta";
+    fixBinary = true;
+    wrapInterpreterInShell = false;
+    matchCredentials = true;
+    magicOrExtension = ''\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x3e\x00'';
+    mask = ''\xff\xff\xff\xff\xff\xfe\xfe\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff'';
+  };
 }
