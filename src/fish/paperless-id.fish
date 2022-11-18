1e7af7bcf29b5406fa658de84eb683cb389e5fe3
Enno Richter <enno@nerdworks.de>
2022-11-18 11:17:27 +0100

    paperless-id: argparse

--- src/fish/paperless-id.fish
+++ src/fish/paperless-id.fish
@@ -1,12 +1,21 @@
-function paperless-id -a type -d "select id of a paperless relation"
-    if test -z "$type"
-        echo "No type specified"
+function paperless-id -d "select id of a paperless relation"
+
+    argparse --name=paperless-id m/multiple t/type= h/help -- $argv
+    or return 1
+
+    if test -n "$_flag_h"
+        echo "Usage: paperless-id [-h] [-m] [-t type]"
+        return
+    end
+
+    if test -z "$_flag_t"
+        echo "Please specify a type"
         return 1
     end
 
-    set -l id (http --check-status --ignore-stdin "localhost:9876/api/$type/" Authorization:@$HOME/.paperless-token \
+    set -l id (http --check-status --ignore-stdin "localhost:9876/api/$_flag_t/" Authorization:@$HOME/.paperless-token \
         | jq -r '.results[] | (.id | tostring) + " " + .name' \
-        | fzf --with-nth 2.. \
+        | fzf --with-nth 2.. $_flag_m \
         | awk '{print $1}')
 
     if test -z "$id"
