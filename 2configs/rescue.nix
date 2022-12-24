385a819ab504836f6f3a2d6bc5654afc1e278213
Enno Richter <enno@nerdworks.de>
2022-12-24 06:13:28 +0100

    add rescue cfg

---
+++ 2configs/rescue.nix
@@ -0,0 +1,32 @@
+{ config, lib, pkgs, modulesPath, ... }:
+{
+  imports = [
+    "${modulesPath}/installer/netboot/netboot.nix"
+    "${modulesPath}/profiles/minimal.nix"
+  ];
+
+  environment.defaultPackages = lib.mkForce [ ];
+
+  users.users.root.openssh.authorizedKeys.keys =
+    let sshPubKeys = import ./users/ssh-pubkeys.nix; in sshPubKeys.authorizedKeys_enno;
+
+  networking.firewall.allowedUDPPorts = [ config.services.tailscale.port ];
+  networking.firewall.checkReversePath = "loose";
+
+  services.tailscale.enable = true;
+
+  systemd.services.tailscale-autoconnect = {
+    description = "Automatic connection to Tailscale";
+
+    after = [ "network-pre.target" "tailscale.service" ];
+    wants = [ "network-pre.target" "tailscale.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig.Type = "oneshot";
+
+    script = ''
+      echo "Authenticating with Tailscale ..."
+      ${pkgs.tailscale}/bin/tailscale up --authkey KEY
+    '';
+  };
+}
