c8c7be29d2faaa211703c7a18f3219ce59b7e678
Enno Richter <enno@nerdworks.de>
2023-01-16 12:55:05 +0100

    update hammerspoon cfg

--- src/hammerspoon/init.lua
+++ src/hammerspoon/init.lua
@@ -4,26 +4,20 @@ hs.hotkey.bind({"cmd", "shift"}, "return", function()
     io.popen("/Users/enno/.nix-profile/bin/alacritty")
 end)
 
-hs.loadSpoon("WindowHalfsAndThirds")
 
--- https://www.hammerspoon.org/Spoons/WindowHalfsAndThirds.html#bindHotkeys
-spoon.WindowHalfsAndThirds:bindHotkeys({
-    left_half = {{"ctrl", "cmd"}, "Left"},
-    right_half = {{"ctrl", "cmd"}, "Right"},
-    top_half = {{"ctrl", "cmd"}, "Up"},
-    bottom_half = {{"ctrl", "cmd"}, "Down"},
-    third_left = {{"ctrl", "alt"}, "Left"},
-    third_right = {{"ctrl", "alt"}, "Right"},
-    third_up = {{"ctrl", "alt"}, "Up"},
-    third_down = {{"ctrl", "alt"}, "Down"},
-    top_left = {{"ctrl", "cmd"}, "1"},
-    top_right = {{"ctrl", "cmd"}, "2"},
-    bottom_left = {{"ctrl", "cmd"}, "3"},
-    bottom_right = {{"ctrl", "cmd"}, "4"},
-    max_toggle = {{"ctrl", "alt", "cmd"}, "f"},
-    max = {{"ctrl", "alt", "cmd"}, "Up"},
-    undo = {{"alt", "cmd"}, "y"},
-    center = {{"alt", "cmd"}, "c"},
-    larger = {{"alt", "cmd", "shift"}, "Right"},
-    smaller = {{"alt", "cmd", "shift"}, "Left"}
-})
+-- meh key:
+hs.hotkey.bind({"control", "shift", "alt"}, "t", function()
+  hs.application.launchOrFocusByBundleID("com.googlecode.iterm2")
+end)
+
+hs.hotkey.bind({"control", "shift", "alt"}, "f", function()
+  app = hs.application.find("org.mozilla.firefox")
+  if app and app:isRunning() then
+    app:activate()
+  else
+    hs.application.launchOrFocusByBundleID("org.mozilla.firefox")  -- always opens new window
+  end
+
+end)
+
+
