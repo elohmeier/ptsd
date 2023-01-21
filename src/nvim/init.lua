9be639d4e2482260a67a9df526df8f9a01a60848
Enno Richter <enno@nerdworks.de>
2023-01-21 11:06:25 +0100

    nvim: formatting

--- src/nvim/init.lua
+++ src/nvim/init.lua
@@ -89,33 +89,33 @@ require("gitsigns").setup {
         end
 
         -- Navigation
-        map('n', ']c', function()
-            if vim.wo.diff then return ']c' end
+        map("n", "]c", function()
+            if vim.wo.diff then return "]c" end
             vim.schedule(function() gs.next_hunk() end)
-            return '<Ignore>'
+            return "<Ignore>"
         end, {expr = true})
 
-        map('n', '[c', function()
-            if vim.wo.diff then return '[c' end
+        map("n", "[c", function()
+            if vim.wo.diff then return "[c" end
             vim.schedule(function() gs.prev_hunk() end)
-            return '<Ignore>'
+            return "<Ignore>"
         end, {expr = true})
 
         -- Actions
-        map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
-        map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
-        map('n', '<leader>hS', gs.stage_buffer)
-        map('n', '<leader>hu', gs.undo_stage_hunk)
-        map('n', '<leader>hR', gs.reset_buffer)
-        map('n', '<leader>hp', gs.preview_hunk)
-        map('n', '<leader>hb', function() gs.blame_line {full = true} end)
-        map('n', '<leader>tb', gs.toggle_current_line_blame)
-        map('n', '<leader>hd', gs.diffthis)
-        map('n', '<leader>hD', function() gs.diffthis('~') end)
-        map('n', '<leader>td', gs.toggle_deleted)
+        map({"n", "v"}, "<leader>hs", ":Gitsigns stage_hunk<CR>")
+        map({"n", "v"}, "<leader>hr", ":Gitsigns reset_hunk<CR>")
+        map("n", "<leader>hS", gs.stage_buffer)
+        map("n", "<leader>hu", gs.undo_stage_hunk)
+        map("n", "<leader>hR", gs.reset_buffer)
+        map("n", "<leader>hp", gs.preview_hunk)
+        map("n", "<leader>hb", function() gs.blame_line {full = true} end)
+        map("n", "<leader>tb", gs.toggle_current_line_blame)
+        map("n", "<leader>hd", gs.diffthis)
+        map("n", "<leader>hD", function() gs.diffthis("~") end)
+        map("n", "<leader>td", gs.toggle_deleted)
 
         -- Text object
-        map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+        map({"o", "x"}, "ih", ":<C-U>Gitsigns select_hunk<CR>")
     end
 }
 
