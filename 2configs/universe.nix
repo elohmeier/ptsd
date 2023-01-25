6b2201f92aa640c17ba8e5841db095bca020a1e7
Enno Richter <enno@nerdworks.de>
2023-01-25 21:24:30 +0100

    borg quota

--- 2configs/universe.nix
+++ 2configs/universe.nix
@@ -194,7 +194,7 @@
 
     mb4 = {
       borg.pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMZ6hB+Qs1yKat2Bz6gweo6yOotHVz+z4bi1hrfhgRVC";
-      borg.quota = "1.3T";
+      borg.quota = "1.5T";
       syncthing.id = "BCJ2QQN-P4JUP5E-NS5AUOL-MDQH3XF-CHQ76SE-V3QCNZ6-QRDMENX-UUNBTA6";
       nets.nwvpn = {
         ip4.addr = "191.18.19.58";
