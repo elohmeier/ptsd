a118df02eb00f365e0c4f73ebd1974935c28008c
Enno Richter <enno@nerdworks.de>
2023-01-27 13:36:07 +0100

    update darwin-defaults

--- 2configs/home/darwin-defaults.nix
+++ 2configs/home/darwin-defaults.nix
@@ -1,10 +1,8 @@
 { config, lib, pkgs, ... }: {
 
   targets.darwin.defaults = {
-    NSGlobalDomain = {
-      # faster key repeat
-      InitialKeyRepeat = 15; # default: 68
-      KeyRepeat = 1; # default: 6
+    "com.apple.dock" = {
+      "show-recents" = false;
     };
 
     finder = {
@@ -12,6 +10,14 @@
       FXDefaultSearchScope = "SCcf"; # current folder
       FXEnableExtensionChangeWarning = false;
       FXPreferredViewStyle = "Nlsv"; # list view
+      FXRemoveOldTrashItems = true;
+      _FXSortFoldersFirst = true;
+    };
+
+    NSGlobalDomain = {
+      # faster key repeat
+      InitialKeyRepeat = 15; # default: 68
+      KeyRepeat = 1; # default: 6
     };
   };
 }
