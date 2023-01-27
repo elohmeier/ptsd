cc486be39970d4219dfa3614bd140c8c53eedab0
Enno Richter <enno@nerdworks.de>
2023-01-27 13:39:21 +0100

    nixos neovim cfg WIP

---
+++ 2configs/neovim.nix
@@ -0,0 +1,23 @@
+{ config, lib, pkgs, ... }:
+
+let
+  pluginPack = pkgs.vimUtils.packDir {
+    mypack = {
+      start = with pkgs.vimPlugins; [
+        # TODO
+      ];
+    };
+  };
+in
+{
+  environment.systemPackages = [
+    pkgs.neovim
+  ];
+
+  environment.etc."xdg/nvim/sysinit.vim".text = ''
+    set runtimepath^=${pluginPack}
+    lua dofile('${../src/nvim/init.lua}')
+  '';
+
+  environment.variables.EDITOR = "nvim";
+}
