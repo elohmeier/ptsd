22af2339bf4020dc5e0e65956d9adb97c156a183
Enno Richter <enno@nerdworks.de>
2023-01-31 21:12:25 +0100

    neovim: intl hotkeys

--- src/nvim/init.lua
+++ src/nvim/init.lua
@@ -134,6 +134,23 @@ vim.api.nvim_set_keymap("n", "<leader>m", "<cmd>enew<cr>", {noremap = true, sile
 
 vim.api.nvim_set_keymap("n", "<leader>b", "<cmd>make<cr><cr>", {noremap = true, silent = true})
 
+-- Option + Shift + w to insert „ (like in macOS)
+vim.api.nvim_set_keymap("i", "<M-S-w>", "„", {noremap = true, silent = true})
+
+-- Option + [ to insert “ (like in macOS)
+vim.api.nvim_set_keymap("i", "<M-[>", "“", {noremap = true, silent = true})
+
+-- Option + u followed by a,o,u,A,O,U to insert ä,ö,ü,Ä,Ö,Ü (like in macOS)
+vim.api.nvim_set_keymap("i", "<M-u>a", "ä", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("i", "<M-u>o", "ö", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("i", "<M-u>u", "ü", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("i", "<M-u>A", "Ä", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("i", "<M-u>O", "Ö", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("i", "<M-u>U", "Ü", {noremap = true, silent = true})
+
+-- Option + s insert ß (like in macOS)
+vim.api.nvim_set_keymap("i", "<M-s>", "ß", {noremap = true, silent = true})
+
 -- tab navigation
 vim.api.nvim_set_keymap("n", "tn", "<cmd>tabnew<cr>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "td", "<cmd>tabclose<cr>", {noremap = true, silent = true})
