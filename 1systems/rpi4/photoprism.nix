964acf674a08b29da84d51059d1f1be94b8dd81c
Enno Richter <enno@nerdworks.de>
2023-02-10 11:21:29 +0100

    rpi4: daily photoprism indexing

--- 1systems/rpi4/photoprism.nix
+++ 1systems/rpi4/photoprism.nix
@@ -1,11 +1,41 @@
 { config, pkgs, ... }:
 
+let
+  environment = {
+    PHOTOPRISM_HTTP_HOST = "127.0.0.1";
+    PHOTOPRISM_HTTP_PORT = toString config.ptsd.ports.photoprism;
+    PHOTOPRISM_SITE_CAPTION = "PhotoPrism";
+    PHOTOPRISM_SITE_URL = "https://rpi4.pug-coho.ts.net:2342/";
+    PHOTOPRISM_TRUSTED_PROXY = "127.0.0.0/8";
+
+    PHOTOPRISM_CACHE_PATH = "/var/lib/photoprism/cache";
+    PHOTOPRISM_IMPORT_PATH = "/var/lib/syncthing/photos/import";
+    PHOTOPRISM_ORIGINALS_PATH = "/var/lib/syncthing/photos/originals";
+    PHOTOPRISM_STORAGE_PATH = "/var/lib/photoprism/storage";
+
+    PHOTOPRISM_DATABASE_DRIVER = "mysql";
+    PHOTOPRISM_DATABASE_NAME = "photoprism";
+    PHOTOPRISM_DATABASE_SERVER = "/run/mysqld/mysqld.sock";
+    PHOTOPRISM_DATABASE_USER = "syncthing";
+  };
+
+  after = [ "network.target" "mysql.service" ];
+  wants = [ "network.target" "mysql.service" ];
+
+  serviceConfig = {
+    User = "syncthing";
+    Group = "syncthing";
+
+    # indexing should be done in the background
+    # might be better to split indexing and serving into two services for different priorities
+    CPUSchedulingPolicy = "idle";
+    IOSchedulingClass = "idle";
+  };
+in
 {
   systemd.services.photoprism = {
     description = "Photoprism Photo Management";
-    after = [ "network.target" "mysql.service" ];
-    wants = [ "network.target" "mysql.service" ];
-
+    inherit environment after wants serviceConfig;
     script = ''
       PWFILE=/var/lib/photoprism/.initial-password.txt
 
@@ -18,35 +48,17 @@
         --admin-password $(cat $PWFILE) \
         start
     '';
+  };
 
-    environment = {
-      PHOTOPRISM_HTTP_HOST = "127.0.0.1";
-      PHOTOPRISM_HTTP_PORT = toString config.ptsd.ports.photoprism;
-      PHOTOPRISM_SITE_CAPTION = "PhotoPrism";
-      PHOTOPRISM_SITE_URL = "https://rpi4.pug-coho.ts.net:2342/";
-      PHOTOPRISM_TRUSTED_PROXY = "127.0.0.0/8";
-
-      PHOTOPRISM_CACHE_PATH = "/var/lib/photoprism/cache";
-      PHOTOPRISM_IMPORT_PATH = "/var/lib/syncthing/photos/import";
-      PHOTOPRISM_ORIGINALS_PATH = "/var/lib/syncthing/photos/originals";
-      PHOTOPRISM_STORAGE_PATH = "/var/lib/photoprism/storage";
-
-      PHOTOPRISM_DATABASE_DRIVER = "mysql";
-      PHOTOPRISM_DATABASE_NAME = "photoprism";
-      PHOTOPRISM_DATABASE_SERVER = "/run/mysqld/mysqld.sock";
-      PHOTOPRISM_DATABASE_USER = "syncthing";
-    };
-
-    serviceConfig = {
-      User = "syncthing";
-      Group = "syncthing";
-
-      # indexing should be done in the background
-      # might be better to split indexing and serving into two services for different priorities
-      CPUSchedulingPolicy = "idle";
-      IOSchedulingClass = "idle";
-    };
-
+  systemd.services.photoprism-index = {
+    description = "Photoprism Indexing";
+    inherit environment after wants serviceConfig;
+    script = ''
+      ${pkgs.photoprism}/bin/photoprism \
+        --sponsor \
+        index
+    '';
+    startAt = "*-*-* 05:30:00";
   };
 
   services.mysql = {
