8b4ea863cfac600bd432d14e1d4cd30d75b64b8a
Enno Richter <enno@nerdworks.de>
2024-06-09 07:15:14 +0200

    htz1: git/ssh passforios compat

--- modules/nixos/hosts/htz1/modules/git.nix
+++ modules/nixos/hosts/htz1/modules/git.nix
@@ -7,4 +7,14 @@ _:
     group = "git";
     user = "git";
   };
+
+  services.openssh.settings.Macs = [
+    # defaults
+    "hmac-sha2-512-etm@openssh.com"
+    "hmac-sha2-256-etm@openssh.com"
+    "umac-128-etm@openssh.com"
+
+    # added for passforios
+    "hmac-sha2-512"
+  ];
 }
