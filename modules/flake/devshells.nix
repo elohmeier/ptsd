aa4c139c6726f1f1f24f102e421836d238703a5f
Enno Richter <enno@nerdworks.de>
2024-06-09 07:14:10 +0200

    fix devshell for macOS

--- modules/flake/devshells.nix
+++ modules/flake/devshells.nix
@@ -1,11 +1,12 @@
-{ inputs, ... }:
+{ ... }:
 {
-  perSystem = { pkgs, system, ... }: {
+  perSystem = { lib, pkgs, system, ... }: {
     devShells.default =
       pkgs.mkShellNoCC {
         packages = [
           pkgs.checkSSLCert
           pkgs.dig
+        ] ++ lib.optionals pkgs.stdenv.isLinux [
           pkgs.hash-slinger
         ];
       };
