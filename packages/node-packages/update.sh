55db0508484881c374fe2e37510323a07ab2a99f
Enno Richter <enno@nerdworks.de>
2024-10-29 15:47:59 +0100

    run formatter

--- packages/node-packages/update.sh
+++ packages/node-packages/update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 node2nix \
-    -i node-packages.json \
-    -o node-packages.nix \
-    -c composition.nix \
-    --pkg-name nodejs_18
+  -i node-packages.json \
+  -o node-packages.nix \
+  -c composition.nix \
+  --pkg-name nodejs_18
