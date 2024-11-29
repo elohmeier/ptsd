49a81df5d7f2dd378e23b35994acf91791c8dc5f
Enno Richter <enno@nerdworks.de>
2024-11-29 07:14:16 +0100

    nix flake update

--- flake.nix
+++ flake.nix
@@ -115,7 +115,6 @@
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.nixpkgs-stable.follows = "nixpkgs";
     };
     systems = {
       url = "github:nix-systems/default";
